669aa300b1da556fe9ea95a6e7376e737d7d6c6b
yahiaboudah <yahiabouda@hotmail.com>
2021-08-15 19:32:33 +0100

    move utils into utjsx

--- u/uijsx/elements/widget.jsx
+++ u/uijsx/elements/widget.jsx
@@ -1,47 +1,25 @@
 
-Object.create = function (proto) {
-
-    function F() {}
-    F.prototype = proto;
-
-    return new F();
-};
-
 Object.extends = function (child, parent) {
-    child.prototype = Object.create(parent.prototype);
+    
+    function F() {}
+    F.prototype = parent.prototype;
+    
+    child.prototype = new F();
 };
 
 _Widget = function _Widget(cfg){
     this.type = "widget";
+    this.x      = 0;
+    this.y      = 0;
+    this.width  = 10;
+    this.height = 10;
 }
 
-_Widget.prototype.showme = function(){
-    return "my type is proudly: " + this.type;
-}
-_Widget.prototype.updateme = function(){
-    return "my type is proudly: " + this.type;
-}
-
+_Window = function _Window(cfg){
+    
+}; Object.extends(_Window, _Widget);
 
 _Button = function _Button(cfg){
-    this.type = "button";
     _Widget.call(this);
+    this.type = "button";
 }; Object.extends(_Button, _Widget);
-
-_Button.prototype.showme = function(){
-    return "A proud button";
-}
-
-
-w = new _Widget({hi: "greetings"});
-b = new _Button({hi: "newButton I am!"});
-
-// $.writeln(w.__proto__.toSource());
-$.writeln(b.toSource());
-
-$.writeln(w.showme());
-try{
-    $.writeln(b.showme());
-}catch(e){
-    $.writeln(e);
-}

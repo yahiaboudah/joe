b27e9dd0956dc93596a2989dab24a372e20a6f1b
yahiaboudah <yahiabouda@hotmail.com>
2021-08-16 14:32:26 +0100

    added wintype and onClose functions

--- u/uijsx/elements/window.jsx
+++ u/uijsx/elements/window.jsx
@@ -3,21 +3,34 @@
 _Window = function _Window(cfg){
     _Container.call(this, cfg, "window");
 
-    this.win = new Window(this.type);
+    // Window properties:
+    this.wintype = this.assign(this.wintype, "palette", ["string"],
+                    function(t){
+                        if(t != "window"
+                        && t != "palette"
+                        && t != "dialog"){
+                            return true;
+                        }
+                    });
+
+    this.win = new Window(this.wintype);
     
     this.win.preferredSize.width = 
                 this.assign(this.width,200, undefined,
-                function(x){return x<=0;});
+                function(x){return x<=10;});
     
     this.win.preferredSize.height = 
                 this.assign(this.height,200, undefined,
-                function(x){return x<=0});
+                function(x){return x<=10});
 
-    this.win.text = this.assign(this.title, "untitled");
+    this.win.text = this.assign(this.title, "untitled", ["string"]);
     this.win.orientation = this.assign(this.orientation, "column");
     this.win.alignChildren = this.assign(this.alignChildren, ["left", "center"]);
     this.win.spacing = this.assign(this.spacing, 0);
     this.win.margins = this.assign(this.margins,0);
+    
+    // Event handling: 
+    this.win.onClose = this.assign(this.onClose, undefined, ["function"]);
 
 }; Object.extends(_Window, _Container);
 
@@ -33,11 +46,13 @@ _Window.prototype.center = function(){
     this.win.center();
 }
 
+_Window.prototype.setOnClose = function(fn){
+    if(typeof fn != "function") return;
+    this.win.onClose = fn;
+}
+
 ww = new _Window({
-    type: "palette",
-    // title: "My script",
-    // width: 400,
-    // height: 400,
+    wintype: "palette",
 });
 
 ww.show();

b8cf41b6058f9cafe6525ee5fa3cb13b95e98146
yahiaboudah <yahiabouda@hotmail.com>
2021-08-20 23:07:20 +0100

    add more Window props

--- u/uijsx/containers/window.jsx
+++ u/uijsx/containers/window.jsx
@@ -3,6 +3,9 @@
 _Window = function _Window(cfg){
     _Container.call(this, cfg, "window");
 
+
+    var evHandlingFuncs = ["Close", "Activate", "Move", "Moving", "Resize", "Resizing", "ShortcutKey", "Show"];
+
     // Window properties:
     this.wintype = this.assign(this.wintype, "palette", ["string"],
                     function(t){
@@ -15,6 +18,7 @@ _Window = function _Window(cfg){
 
     this.win = new Window(this.wintype);
     
+    // size:
     this.win.preferredSize.width = 
                 this.assign(this.width,200, undefined,
                 function(x){return x<=10;});
@@ -23,16 +27,47 @@ _Window = function _Window(cfg){
                 this.assign(this.height,200, undefined,
                 function(x){return x<=10});
 
+    // props:
     this.win.text = this.assign(this.title, "untitled", ["string"]);
     this.win.orientation = this.assign(this.orientation, "column");
-    this.win.alignChildren = this.assign(this.alignChildren, ["left", "center"]);
+    this.win.alignChildren = this.assign(this.alignChildren, ["left", "center"], ["array", "string"]);
     this.win.spacing = this.assign(this.spacing, 0);
     this.win.margins = this.assign(this.margins,0);
-    
-    // Event handling: 
-    this.win.onClose = this.assign(this.onClose, undefined, ["function"]);
+    this.win.resizeable = this.assign(this.resizeable, false);
+    this.win.closeButton = this.assign(this.closeButton, true);
+    this.win.minimizeButton = this.assign(this.minimizeButton, false);
+    this.win.maximizeButton = this.assign(this.maximizeButton, false);
+    this.win.independent = this.assign(this.independent, false);
+    this.win.borderless = this.assign(this.borderless, false);
+
+    // Event handling:
+    for(i=-1; ++i<evHandlingFuncs.length;)
+    {
+        f = "on" + (f = evHandlingFuncs[i]);
+        this.win[f] = this.assign(this[f], undefined, ["function"]);
+    }
 
 }; Object.extends(_Window, _Container);
+
+_Window.alert = function(cfg)
+{
+    Window.alert(cfg.msg, cfg.title, cfg.errIcon);
+}
+
+_Window.confirm = function(cfg)
+{
+    Window.confirm(cfg.msg, cfg.defNo, cfg.title);
+}
+
+_Window.find = function(cfg)
+{
+    Window.find(cfg.type, cfg.title);
+}
+
+_Window.prompt = function(cfg)
+{
+    Window.prompt(cfg.msg, cfg.defValue, cfg.title);
+}
 
 _Window.prototype.show = function(){
     this.win.show();
@@ -45,14 +80,3 @@ _Window.prototype.close = function(){
 _Window.prototype.center = function(){
     this.win.center();
 }
-
-_Window.prototype.setOnClose = function(fn){
-    if(typeof fn != "function") return;
-    this.win.onClose = fn;
-}
-
-ww = new _Window({
-    wintype: "palette",
-});
-
-ww.show();
---
+++ u/uijsx/layout/test.jsx
@@ -0,0 +1,22 @@
+
+
+w = new Window("palette", "some of my shit");
+
+w.orientation = "row";
+
+p = w.add("panel", undefined ,"myPanel");
+p.spacing = 250;
+p.orientation = "row";
+p.alignChildren = "bottom"
+
+b = p.add("button");
+b.preferredSize = [100, 5];
+b.alignment = "top";
+p.add("button");
+
+g =w.add("panel", undefined, "g panel")
+
+g.add("button")
+g.add("button");
+
+w.show();

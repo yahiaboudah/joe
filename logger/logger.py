0200da5f7f484a28f5bdb081c06606482a23181e
yahiaboudah <yahiabouda@hotmail.com>
2021-08-23 19:44:01 +0100

    create logger

---
+++ logger/logger.py
@@ -0,0 +1,12 @@
+
+class Being():
+    def __init__(self) -> None:
+        pass
+
+class Object():
+    def __init__(self) -> None:
+        pass
+
+class Logger():
+     def __init__(self, B):
+         self.b = B

17f53723327b0f8ef3009772afa7d0631f9a8538
yahiaboudah <yahiabouda@hotmail.com>
2021-08-30 15:18:47 +0100

    15 props, 9 tp go

--- ujsx/uijsx/tablemaker.jsx
+++ ujsx/uijsx/tablemaker.jsx
@@ -19,14 +19,20 @@ new Table(Table.process(
      "type(RO), string, \"button\", \"button\", static property that stores widget type",
      "window(RO), object, Window({}), Window({}), static property that calls the window of the button",
 
-     "alignment, string, center, [center,left,right], align within the parent",
-     "textDirection, string, null, [ltr, rtl], specifies the text direction \n(TESTED. DOES NOT WORK)",
+     "alignment, string, center, [\"center\";\"left\";\"right\"], align within the parent",
+     "textDirection, string, null, [\"ltr\"; \"rtl\"], specifies the text direction \n(TESTED. DOES NOT WORK)",
      
      "children, array, [], any:arr, An array of child elements",
      "properties, object, {}, any:object, An object with user-assigned property values",
 
      // location stuff:
-     
+    // bounds = "bounds";
+    "bounds, object, {x:0; y:0; \nwidth:0; height:0}, {x:x; y:y;\n width:w; height:h}, The bounds of this button",
+    // location = "location";
+    "location, "
+    // maximumSize=  400;
+    // minimumSize = 400;
+    // preferredSize = [50,50],
 
 )).write($.fp, 3)
 

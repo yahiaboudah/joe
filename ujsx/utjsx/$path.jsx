38423ce7e1b96dc13e74deeee62cd41daca97f87
yahiaboudah <yahiabouda@hotmail.com>
2021-09-01 23:22:19 +0100

    add mkdir() and exists() to Path prototype

--- ujsx/utjsx/$path.jsx
+++ ujsx/utjsx/$path.jsx
@@ -8,16 +8,25 @@ Path.prototype.resolve = function(){
     return this.parts.join("/");
 }
 
+Path.prototype.exists = function(){
+    return Folder(this.resolve()).exists;
+}
+
+Path.prototype.mkdir = function(){
+    if(this.exists()) return false;
+    return Folder(this.resolve()).create();
+}
+
 Path.prototype["/"] = function(op){
     
     curr = this;
     cStr = curr.resolve();
     newPath = [cStr, op].join("/");
 
-    return typeof this.constructor;
+    return new Path(newPath);
 }
 
-pp = new Path("c:/hello");
-ps = pp / "newdir";
+pp = new Path("d:/media");
+ps = pp / "Memesplusplus";
 
-$.writeln(ps)
+$.writeln(ps.mkdir())

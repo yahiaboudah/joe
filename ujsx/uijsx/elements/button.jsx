e870e8fff1c15a0fb48397e6cfccb7aa8ef4780c
yahiaboudah <yahiabouda@hotmail.com>
2021-09-02 13:02:49 +0100

    update button class

--- ujsx/uijsx/elements/button.jsx
+++ ujsx/uijsx/elements/button.jsx
@@ -120,6 +120,31 @@
 
 //@include "../blocks/widget.jsx"
 
-_Button = function _Button(cfg){
+_Button = function _Button(cfg){	
     _Widget.call(this, cfg, "button");
+
+	// validate/ sanitize arguments before passing them to this;
+	//=========================================================
+
+	this.characters = 0;
+	this.text = "";
+	this.justify = "center";
+	this.shortcutKey = "";
+	this.enabled = 1;
+	this.active = 0;
+	this.visible = 1;
+	this.helpTip = "";
+	this.properties = {};
+	this.indent = 0;
+	this.alignment = "center";
+
+	//========================
+	this.eventListeners = [
+		// [eventName:str, function handler(){...}:func, capturePhase:bool]
+		// ...
+	]
+	this.onActivate = cfg.onActivate;
+	this.onDeactivate = cfg.onDeactivate;
+	this.onShortcutKey = cfg.onShortcutKey;
+
 }; Object.extends(_Button, _Widget);

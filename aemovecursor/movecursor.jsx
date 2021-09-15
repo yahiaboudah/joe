8ddd6b344e8f39877bf77d633662d669bc3de4e6
yahia boudah <yahiabouda@hotmail.com>
2021-09-15 16:55:11 +0100

    Create movecursor.jsx

---
+++ aemovecursor/movecursor.jsx
@@ -0,0 +1,32 @@
+w = new _Window({
+  title: "Shifter",
+  resizeable: true,
+  
+  children: [
+    new _Button({
+      text: "<-",
+      onClick: function(){
+        if(c.time <= this.state.timeShift) c.time =0;
+        else c.time = c.time + this.state.timeShift;
+      }
+    }),
+    new _Button({
+      text: "set",
+      onClick: function(){
+        this.state.timeShift = $prompt("Enter new timeshift value", function(x){
+          return parseFloat(x);
+        })
+      }
+    }),
+    new _Button({
+      text: "->",
+      onClick: function(){
+        fTime = c.time + this.state.timeShift;
+        if(fTime > c.duration) c.time = c.duration;
+        else c.time = fTime;
+      }
+    }),
+  ]
+})
+
+w.show();

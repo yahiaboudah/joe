a1203c394eda9a3c5cfbb5364a7634caa5492c97
yahiaboudah <yahiabouda@hotmail.com>
2021-09-18 23:43:19 +0100

    udpate iconButton

--- ujsx/uijsx/test3.jsx
+++ ujsx/uijsx/test3.jsx
@@ -1,35 +1,80 @@
 
 
-w = new Window("palette");
-b = w.add("iconbutton", undefined, 
+Function.prototype.bind = Function.prototype.bind || function bind(thisArg) {
+	var method = this;
+	var args = Array.prototype.slice.call(arguments, 1);
+
+	return function bound() {
+		var _args = args.concat(Array.prototype.slice.call(arguments));
+		if (!(this instanceof bound))
+			return method.apply(thisArg, _args);
+
+		var __args = [];
+		for (var i = 0, len = _args.length; i < len; i++)
+			__args.push('_args[' + i + ']');
+
+		return eval('new method(' + __args.join(',') + ')');
+	};
+}
+
+function iconButton(cfg)
+{
+    var img = cfg.img.standard;
+
+    var b = this.add("iconbutton", undefined, 
     
     ScriptUI.newImage(
-        "/d/icons/img/sova.png", // default
-        "/d/icons/img/kj.png", // onDisabled (b.enabled = false)
-        "/d/icons/img/kj.png", //onClick
-        "/d/icons/img/sova.png" // mouseOver (rollover state)
-    ), 
-    
+        img, // default
+        cfg.img.disabled || img, // onDisabled (b.enabled = false)
+        cfg.img.clicked  || img, //onClick
+        cfg.img.hover    || img // mouseOver (rollover state)
+    ),
     {
+        style: cfg.style
+    });
+
+    b.onClick = cfg.onClick;
+    b.alignment = cfg.alignment;
+    b.helpTip = cfg.helpTip;
+
+    listen = cfg.eventListeners;
+
+    for(evType in listen) 
+    {
+        if(!listen.hasOwnProperty(evType)) continue;
+        for(ev in listen[evType]) 
+        {
+            if(!listen[evType].hasOwnProperty(ev)) continue;
+            b.addEventListener(evType, listen[evType][ev].bind(b))
+        }
+    }
+}
+
+w = new Window("palette");
+
+iconButton.call(w,{
+    img: 
+    {
+        standard:  "/d/icons/img/sova.png", 
+        disabled: "/d/icons/img/kj.png",
+        clicked:  "/d/icons/img/kj.png",
+        hover:    "/d/icons/img/sova.png"
+    },
+    
     style: "toolbutton",
-});
+    onClick: function(){},
+    alignment: ["center", "top"],
+    helpTip  :"test iconbutton",
 
-b.onClick   = clearConsole;
-b.alignment = ["center", "top"];
-b.helpTip   = "Clear all console data!";
-
-b.addEventListener("mousedown", function(){
-    app.setTimeout(function(){
-        b.active = !b.active;
-        c.active = true;
-    })
+    eventListeners: {
+        "mousedown": [
+            function(){
+                app.setTimeout((function(){
+                    alert(this.helpTip);
+                }).bind(this), 2000)
+            }
+        ]
+    }
 })
-
-c = w.add("edittext", undefined, "test",
-{
-    multiline: true, 
-    scrollbars: true,
-});
-c.preferredSize = [150, 100];
 
 w.show();

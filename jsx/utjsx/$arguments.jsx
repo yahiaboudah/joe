103cd5bab1db206a7652f337d8b08c99bd166b88
yahiaboudah <yahiabouda@hotmail.com>
2021-09-25 16:30:42 +0100

    restructure Arguments

--- jsx/utjsx/$arguments.jsx
+++ jsx/utjsx/$arguments.jsx
@@ -10,101 +10,104 @@
 		Created:        2106 (YYMM)
 		Modified:       2107 (YYMM)
 *******************************************************************************/
-//@include "$string.jsx"
-/******************************************************************************/
 
-function Arguments() {};
+(function Arguments(g, A){
 
-/*
-* Get a function args (eg. Arguments.getArgs(Arguments.getArgs) => ["c", "nocom"])
-*/
-Arguments.getArgs = function(c, nocom)
-{
+        //@include "$string.jsx"
+        g[A] = A;
 
-  if(typeof nocom == "undefined") nocom =true;
-  return  c.toString().split(/\)\/*/)[0]
-                      .replace(/^[^(]*[(]/, '')  
-                      .replace(/\s+/g, '')
-                      .replace(nocom?/\/\*([\s\S]*?)\*\//g:'','')
-                      .split(',');
-}
-
-/**
- * Extract parameter names and types from a function definition.
- * @param {String} func The function string  
- */
-Arguments.params = function( /*String*/ func) {
-
-        var paramsList = Arguments.getArgs(func);
-
-        for (var k = 0, len = params.length; k < len; k++) {
-
-                var currParam = params[k],
-                    split     = currParam.split("*/"),
-                    s0        = split[0],
-                    s1        = split[1];
-
-                params[k] = s0.slice(0, 2) == "/*"?
-                            "{0}:{1}".f(s0.slice(2), s1):
-                            "Any:{0}".f(s0);
+        A.getArgs = function(c, nocom)
+        /*
+        * Get a function args (eg. Arguments.getArgs(Arguments.getArgs) => ["c", "nocom"])
+        */
+        {  
+             if(typeof nocom == "undefined") nocom =true;
+             return c.toString().split(/\)\/*/)[0]
+                                .replace(/^[^(]*[(]/, '')  
+                                .replace(/\s+/g, '')
+                                .replace(nocom?/\/\*([\s\S]*?)\*\//g:'','')
+                                .split(',');                
         }
 
-
-        return paramsList;
-}
-/**
- * 
- * @param {object} args the function arguments
- * @param {Boolean} limitArgs the name of the function: optional parameter.
- * @returns {0}  if params are healthy
- */
-Arguments.check = function( /*Object*/ args, /*Boolean*/ optArgs, /*Boolean*/ limitArgs) {
-        
-
-        const ERRS = 
+        A.params = function(/*String*/ func)
+        /** 
+        * Extract parameter names and types from a function definition.
+        * @param {String} func The function string  
+        */
         {
-            BAD_ARG     :  "Bad Argument Error."
-                         + "Arg {0} is a {1} and not a {2}",
-            MISSING_ARG : "Missing Argument Error.",
-            EXTRA_ARG   : "Extra Argument Error."
+                var paramsList = Arguments.getArgs(func);
+
+                for (var k = 0, len = params.length; k < len; k++) {
+        
+                        var currParam = params[k],
+                            split     = currParam.split("*/"),
+                            s0        = split[0],
+                            s1        = split[1];
+        
+                        params[k] = s0.slice(0, 2) == "/*"?
+                                    "{0}:{1}".f(s0.slice(2), s1):
+                                    "Any:{0}".f(s0);
+                }
+        
+        
+                return paramsList;
         }
 
-        var definedAndBool = function(myArg){
-                return ((myArg !== undefined) || (myArg.constructor === Boolean));
-        }
+        A.check  = function(/*Object*/ args, /*Boolean*/ optArgs, /*Boolean*/ limitArgs)
+        /**
+        * 
+        * @param {object} args the function arguments
+        * @param {Boolean} limitArgs the name of the function: optional parameter.
+        * @returns {0}  if params are healthy
+        */
+        {
+                                
 
-        if (!definedAndBool(limitArgs)) limitArgs = true;
-        if (!definedAndBool(optArgs))   optArgs   = false;
-
-
-        var stack      = $.stack.split("\n");
-            funcName   = stack[stack.length - 3].split("(")[0],
-            funcParams = Arguments.params(eval(funcName).toString()),
-            isGreater  = (args.length > funcParams.length),
-            isLess     = (args.length < funcParams.length);
-
-        if (isGreater && limitArgs) throw Error(ERRS.EXTRA_ARG);
-        if (isLess    && !optArgs ) throw Error(ERRS.MISSING_ARG);
-
-        // Args length has priority over params length
-        for (var i = 0; i < args.length; i++) {
-
-                var split     = funcParams[i].split(":"),
-                    type      = split[0].toLowerCase(),
-                    paramName = split[1],
-                    argValue  = args[i],
-                    argType;
-
-                if ([null, undefined].includes(argValue)) continue;
-
-                argType = argValue.constructor.name.toLowerCase();
-                if (!["any", argType].includes(type)) 
+                const ERRS = 
                 {
-                        throw Error(ERR.BAD_ARG.f(
-                                "{0}[1]".f(paramName, i),
-                                argType,
-                                type
-                        ));
+                BAD_ARG     :  "Bad Argument Error."
+                                + "Arg {0} is a {1} and not a {2}",
+                MISSING_ARG : "Missing Argument Error.",
+                EXTRA_ARG   : "Extra Argument Error."
+                }
+
+                var definedAndBool = function(myArg){
+                        return ((myArg !== undefined) || (myArg.constructor === Boolean));
+                }
+
+                if (!definedAndBool(limitArgs)) limitArgs = true;
+                if (!definedAndBool(optArgs))   optArgs   = false;
+
+
+                var stack      = $.stack.split("\n");
+                funcName   = stack[stack.length - 3].split("(")[0],
+                funcParams = Arguments.params(eval(funcName).toString()),
+                isGreater  = (args.length > funcParams.length),
+                isLess     = (args.length < funcParams.length);
+
+                if (isGreater && limitArgs) throw Error(ERRS.EXTRA_ARG);
+                if (isLess    && !optArgs ) throw Error(ERRS.MISSING_ARG);
+
+                // Args length has priority over params length
+                for (var i = 0; i < args.length; i++) {
+
+                        var split     = funcParams[i].split(":"),
+                        type      = split[0].toLowerCase(),
+                        paramName = split[1],
+                        argValue  = args[i],
+                        argType;
+
+                        if ([null, undefined].includes(argValue)) continue;
+
+                        argType = argValue.constructor.name.toLowerCase();
+                        if (!["any", argType].includes(type)) 
+                        {
+                                throw Error(ERR.BAD_ARG.f(
+                                        "{0}[1]".f(paramName, i),
+                                        argType,
+                                        type
+                                ));
+                        }
                 }
         }
-}
+})($.global, {toString: function(){return "Arguments"}});

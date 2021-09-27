9762991c1bab58363c47821a33d78c8f09f646df
yahiaboudah <yahiabouda@hotmail.com>
2021-09-27 20:48:38 +0100

    initialize redux

--- jsx/uijsx/state/redux.jsx
+++ jsx/uijsx/state/redux.jsx
@@ -1,9 +1,72 @@
+const { func } = require("prop-types");
 
 
-(function Redux(g, R){
+(function Reduxx(g, Redux){
 
-    g[R] = R;
+    g[Redux] = Redux;
 
-    // R.
+
+    STORE = {
+        getState : function()
+        {
+
+        },
+
+        dispatch : function()
+        {
+            
+        }
+    }
+
+    Redux.combineReducers = function(reducers)
+    {
+        var keys = Object.keys(reducers);
+
+        return function(state, action) 
+        {
+            var totalState = {};
+            state = state || {};
+            
+            keys.forEach(function(key)
+            {
+              totalState[key] = reducers[key](state[key], action);
+            });
+            
+            return totalState;
+        };
+    },
+
+    Redux.createStore     = function(reducer)
+    {
+        var state;
+        var listeners = [];
+
+        function getState() {
+            return state;
+        }
+
+        function subscribe(listener) {
+            listeners.push(listener);
+            return function unsubscribe() {
+            listeners.splice(listeners.indexOf(listener), 1);
+            };
+        }
+
+        function dispatch(action) {
+            state = reducer(state, action);
+            listeners.forEach(function(listener) {
+            listener();
+            });
+            return action;
+        }
+
+        return {
+            getState: getState,
+            subscribe: subscribe,
+            dispatch: dispatch,
+        };
+    },
+
+    Redux.
 
 })($.global, {toString: function(){return "Redux"}})

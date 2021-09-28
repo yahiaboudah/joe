1d5e1723f1bc7816c8b88d32e14d0b12316775d1
yahiaboudah <yahiabouda@hotmail.com>
2021-09-28 22:25:12 +0100

    write a simple tester function for $sys

--- jsx/$sys.jsx
+++ jsx/$sys.jsx
@@ -27,3 +27,56 @@
     }
 
 })($.global, {toString: function(){return "sys"}})
+
+
+var Xester = 
+{
+    currFunc: function(){},
+
+    describe: function(fn, testo){
+        
+        this.currFunc = fn;
+
+        for(test in testo) if(testo.hasOwnProperty(test))
+        {
+            testf = testo[test];
+            testf.call(this);        
+        }
+    },
+
+    expect: function(input, cb){
+        
+        if(typeof cb != "function") cb = function(x){return x};
+        input  = cb.apply(null, inputArr);
+        result = this.currFunc.apply(null, inputArr);
+        
+        return {
+            result: result,
+            toGive: function(whatGives){
+                return this.result == whatGives;
+            }
+        }
+
+    }
+}
+
+(function systest(host, self, T){
+
+    host.test = function(write)
+    {
+        if(typeof write == "undefined") write = true;
+    }
+
+    T.describe(host.cmd, {
+    
+        "should return python version": function(){with(this){
+            
+            $.global.myGlobalShit = expect(["python --version"]).toGive("Python 3.9.6");
+        }},
+    });
+
+})($.global.sys, {toString: function(){return "test"}}, Xester)
+
+
+sys.test();
+$.writeln($.global.myGlobalShit)

f4989e9c9b63b6bd26e2d9d50767ccededf103f5
yahiaboudah <yahiabouda@hotmail.com>
2021-10-05 14:11:42 +0100

    reorganize file extens

--- jsx/$file.jsx
+++ jsx/$file.jsx
@@ -1,225 +1,232 @@
 /*******************************************************************************
 		Name:           $file
-		Desc:           Extend the prototype of the File/Folder classes in extendscript.
+		Desc:           File/Folder extensions.
 		Path:           /utils/$file.jsx
-		Require:        String:checkFF, Array:indexOf, $:$sleep
-		Encoding:       ÛȚF8
-		Kind:           Part of the Utils.
-		API:            $open, $close, $create, $write, $read, $clear, $seek,
-                                $execute, $lines, $listenMod, $listenChar, $clearFolder.
-		Todo:           ---
+		API:            $manyfunctions
 		Created:        2106 (YYMM)
-		Modified:       2107 (YYMM)
+		Modified:       2110 (YYMM)
 *******************************************************************************/
 //@include "$$.jsx"
 //@include "$array.jsx"
 //@include "$string.jsx"
 /******************************************************************************/
-File.CATEGORIES = 
-    [
-        "WEB",
-        "CODE",
-        "IMAGE",
-        "VIDEO",
-        "ARCHIV",
-        "TEXT",
-        "AUDIO",
-        "UNKNOWN"
-    ];
-    
-File.TYPES_BY_CATEGORY  = {0:"css less scss wasm ",6:"aac aiff ape au flac gsm it m3u m4a mid mod mp3 mpa pls ra s3m sid wav wma xm ", 1:"c cc class clj cpp cs cxx el go h java lua m m4 php pl po py pyw rb rs swift vb vcxproj xcodeproj xml diff patch html js jsx ","slide":"ppt odp ","sheet":"ods xls xlsx csv ics vcf ",2:"3dm 3ds max bmp dds gif jpg jpeg png psd xcf tga thm tif tiff ai eps ps svg dwg dxf gpx kml kmz webp ",4:"7z a apk ar bz2 cab cpio deb dmg egg gz iso jar lha mar pea rar rpm s7z shar tar tbz2 tgz tlz war whl xpi zip zipx xz pak ","book":"mobi epub azw1 azw3 azw4 azw6 azw cbr cbz ",5:"doc docx ebook log md msg odt org pages pdf rtf rst tex txt wpd wps ","exec":"exe msi bin command sh bat crx ","font":"eot otf ttf woff woff2 ",3:"3g2 3gp aaf asf avchd avi drc flv m2v m4p m4v mkv mng mov mp2 mp4 mpe mpeg mpg mpv mxf nsv ogg ogv ogm qt rm rmvb roq srt svi vob webm wmv yuv "};
-File.TYPES_BY_EXTENSION = {"ogm":3,"doc":5,"class":1,"js":1,"swift":1,"cc":1,"tga":2,"ape":6,"woff2":"font","cab":4,"whl":4,"mpe":3,"rmvb":3,"srt":3,"pdf":5,"xz":4,"exe":"exec","m4a":6,"crx":"exec","vob":3,"tif":2,"gz":4,"roq":3,"m4v":3,"gif":2,"rb":1,"3g2":3,"m4":1,"ar":4,"vb":1,"sid":6,"ai":2,"wma":6,"pea":4,"bmp":2,"py":1,"mp4":3,"m4p":3,"ods":"sheet","jpeg":2,"command":"exec","azw4":"book","otf":"font","ebook":5,"rtf":5,"ttf":"font","mobi":"book","ra":6,"flv":3,"ogv":3,"mpg":3,"xls":"sheet","jpg":2,"mkv":3,"nsv":3,"mp3":6,"kmz":2,"java":1,"lua":1,"m2v":3,"deb":4,"rst":5,"csv":"sheet","pls":6,"pak":4,"egg":4,"tlz":4,"c":1,"cbz":"book","xcodeproj":1,"iso":4,"xm":6,"azw":"book","webm":3,"3ds":2,"azw6":"book","azw3":"book","php":1,"kml":2,"woff":"font","log":5,"zipx":4,"3gp":3,"po":1,"mpa":6,"mng":3,"wps":5,"wpd":5,"a":4,"s7z":4,"ics":"sheet","tex":5,"go":1,"ps":2,"org":5,"sh":"exec","msg":5,"xml":1,"cpio":4,"epub":"book","docx":5,"lha":4,"flac":6,"odp":"slide","wmv":3,"vcxproj":1,"mar":4,"eot":"font","less":0,"asf":3,"apk":4,"css":0,"mp2":3,"odt":5,"patch":1,"wav":6,"msi":"exec","rs":1,"gsm":6,"ogg":3,"cbr":"book","azw1":"book","m":1,"dds":2,"h":1,"dmg":4,"mid":6,"psd":2,"dwg":2,"aac":6,"s3m":6,"cs":1,"cpp":1,"au":6,"aiff":6,"diff":1,"avi":3,"bat":"exec","html":1,"pages":5,"bin":"exec","txt":5,"rpm":4,"m3u":6,"max":2,"vcf":"sheet","svg":2,"ppt":"slide","clj":1,"png":2,"svi":3,"tiff":2,"tgz":4,"mxf":3,"7z":4,"drc":3,"yuv":3,"mov":3,"tbz2":4,"bz2":4,"gpx":2,"shar":4,"xcf":2,"dxf":2,"jar":4,"qt":3,"tar":4,"xpi":4,"zip":4,"thm":2,"cxx":1,"3dm":2,"rar":4,"md":5,"scss":0,"mpv":3,"webp":2,"war":4,"pl":1,"xlsx":"sheet","mpeg":3,"aaf":3,"avchd":3,"mod":6,"rm":3,"it":6,"wasm":0,"el":1,"eps":2}
 
-File.prototype.isOpen = false;
-// Handler
-File.prototype.$open = function(mode) {
+(function FileConstants(){
+        File.CATEGORIES = 
+        [
+                "WEB",
+                "CODE",
+                "IMAGE",
+                "VIDEO",
+                "ARCHIV",
+                "TEXT",
+                "AUDIO",
+                "UNKNOWN"
+        ];
+        
+        File.TYPES_BY_CATEGORY  = {0:"css less scss wasm ",6:"aac aiff ape au flac gsm it m3u m4a mid mod mp3 mpa pls ra s3m sid wav wma xm ", 1:"c cc class clj cpp cs cxx el go h java lua m m4 php pl po py pyw rb rs swift vb vcxproj xcodeproj xml diff patch html js jsx ","slide":"ppt odp ","sheet":"ods xls xlsx csv ics vcf ",2:"3dm 3ds max bmp dds gif jpg jpeg png psd xcf tga thm tif tiff ai eps ps svg dwg dxf gpx kml kmz webp ",4:"7z a apk ar bz2 cab cpio deb dmg egg gz iso jar lha mar pea rar rpm s7z shar tar tbz2 tgz tlz war whl xpi zip zipx xz pak ","book":"mobi epub azw1 azw3 azw4 azw6 azw cbr cbz ",5:"doc docx ebook log md msg odt org pages pdf rtf rst tex txt wpd wps ","exec":"exe msi bin command sh bat crx ","font":"eot otf ttf woff woff2 ",3:"3g2 3gp aaf asf avchd avi drc flv m2v m4p m4v mkv mng mov mp2 mp4 mpe mpeg mpg mpv mxf nsv ogg ogv ogm qt rm rmvb roq srt svi vob webm wmv yuv "};
+        File.TYPES_BY_EXTENSION = {"ogm":3,"doc":5,"class":1,"js":1,"swift":1,"cc":1,"tga":2,"ape":6,"woff2":"font","cab":4,"whl":4,"mpe":3,"rmvb":3,"srt":3,"pdf":5,"xz":4,"exe":"exec","m4a":6,"crx":"exec","vob":3,"tif":2,"gz":4,"roq":3,"m4v":3,"gif":2,"rb":1,"3g2":3,"m4":1,"ar":4,"vb":1,"sid":6,"ai":2,"wma":6,"pea":4,"bmp":2,"py":1,"mp4":3,"m4p":3,"ods":"sheet","jpeg":2,"command":"exec","azw4":"book","otf":"font","ebook":5,"rtf":5,"ttf":"font","mobi":"book","ra":6,"flv":3,"ogv":3,"mpg":3,"xls":"sheet","jpg":2,"mkv":3,"nsv":3,"mp3":6,"kmz":2,"java":1,"lua":1,"m2v":3,"deb":4,"rst":5,"csv":"sheet","pls":6,"pak":4,"egg":4,"tlz":4,"c":1,"cbz":"book","xcodeproj":1,"iso":4,"xm":6,"azw":"book","webm":3,"3ds":2,"azw6":"book","azw3":"book","php":1,"kml":2,"woff":"font","log":5,"zipx":4,"3gp":3,"po":1,"mpa":6,"mng":3,"wps":5,"wpd":5,"a":4,"s7z":4,"ics":"sheet","tex":5,"go":1,"ps":2,"org":5,"sh":"exec","msg":5,"xml":1,"cpio":4,"epub":"book","docx":5,"lha":4,"flac":6,"odp":"slide","wmv":3,"vcxproj":1,"mar":4,"eot":"font","less":0,"asf":3,"apk":4,"css":0,"mp2":3,"odt":5,"patch":1,"wav":6,"msi":"exec","rs":1,"gsm":6,"ogg":3,"cbr":"book","azw1":"book","m":1,"dds":2,"h":1,"dmg":4,"mid":6,"psd":2,"dwg":2,"aac":6,"s3m":6,"cs":1,"cpp":1,"au":6,"aiff":6,"diff":1,"avi":3,"bat":"exec","html":1,"pages":5,"bin":"exec","txt":5,"rpm":4,"m3u":6,"max":2,"vcf":"sheet","svg":2,"ppt":"slide","clj":1,"png":2,"svi":3,"tiff":2,"tgz":4,"mxf":3,"7z":4,"drc":3,"yuv":3,"mov":3,"tbz2":4,"bz2":4,"gpx":2,"shar":4,"xcf":2,"dxf":2,"jar":4,"qt":3,"tar":4,"xpi":4,"zip":4,"thm":2,"cxx":1,"3dm":2,"rar":4,"md":5,"scss":0,"mpv":3,"webp":2,"war":4,"pl":1,"xlsx":"sheet","mpeg":3,"aaf":3,"avchd":3,"mod":6,"rm":3,"it":6,"wasm":0,"el":1,"eps":2}
 
-        var cases = ["r", "w", "a", "e"];
-        idx = cases.indexOf(mode);
+        File.remove = function(){
+        
+                var args = Array.prototype.slice.call(arguments),
+                i    = -1, len = args.length;
+        
+                for(;++i< len;) if(args[i].constructor == File) args[i].remove();
+        }
+})();
 
-        if (idx == -1) {
-                if (this.fsName.checkFF()) with(this) {
-                        open("e");
-                        isOpen = true
-                }
-                else with(this) {
-                        open("w");
-                        isOpen = true
-                };
+(function FileProtoExtens(){
+        
+        File.prototype.isOpen = false;
+        // Handler
+        File.prototype.$open = function(mode) {
+
+                var cases = ["r", "w", "a", "e"];
+                idx = cases.indexOf(mode);
+
+                if (idx == -1) {
+                        if (this.fsName.checkFF()) with(this) {
+                                open("e");
+                                isOpen = true
+                        }
+                        else with(this) {
+                                open("w");
+                                isOpen = true
+                        };
+                        return this;
+                } // throw Error("File open mode is invalid");
+
+                this.open(cases[idx]);
+                this.isOpen = true;
                 return this;
-        } // throw Error("File open mode is invalid");
-
-        this.open(cases[idx]);
-        this.isOpen = true;
-        return this;
-}
-// Handler
-File.prototype.$close = function(){
-        this.isOpen = false;
-        return (this.close(), this);
-}
-// Handler
-File.prototype.$write = function(txt, mode) {
-
-        if (this.isOpen) this.write(txt, mode);
-        return (this.$open(mode).write(txt), this.close());
-}
-// Handler
-File.prototype.$read = function() {
-
-        if (!this.exists) throw Error("Can't read a non-existent file!");
-        
-        var d = this.$open("r").read();
-        this.$close();
-        return d;
-}
-// Handler
-File.prototype.$clear = function(txt) {
-
-        return (this.$write(txt || ""), this);
-}
-// Handler
-File.prototype.$seek = function(pos) {
-
-        if (!this.isOpen) this.$open('r');
-        return (this.seek(pos), this);
-}
-// Handler
-File.prototype.$create = function(text, encoding) {
-
-    this.encoding = encoding || "UTF-8";
-    return (this.$write((text || ""), 'w'), this);
-}
-// Handler
-File.prototype.$execute = function(slp, cb, doClose) {
-
-        this.execute();
-        if(!!doClose) this.$close();
-        $.sleep(slp || 0);
-        if(typeof callback == "function") cb.call(this);
-
-        return this;
-}
-// Info
-File.prototype.$lines = function() {
-
-    var lines = [];
-    this.$open("r");
-
-    while (!this.eof) lines.push(this.readln());
-
-    return (this.$close(), lines);
-}
-// Wait
-File.prototype.$listenForChange = function(debug, wait, maxiter) {
-
-        var iter = -1, maxiter = maxiter || 100;
-
-        while (++iter < maxiter) {
-                if (this.modified > lmod) break;
-                $.$sleep(
-                        !wait? 180: wait == "exp"? Math.round(2, iter+6):
-                        wait,
-                        debug,
-                        iter
-                );
         }
-
-}
-// Wait
-File.prototype.$listenForChar = function(charac, pos, wait, maxiter, debug) {
-
-        var iter = -1, maxiter = maxiter || 100;
-        while (++iter < maxiter) {
-
-                if (this.$open('r').$seek(pos).readch() == charac) break;
-                else $.$sleep(wait, debug, iter);
+        // Handler
+        File.prototype.$close = function(){
+                this.isOpen = false;
+                return (this.close(), this);
         }
-        
-        this.$close();
-}
-File.prototype.$listen = function(delay, debug, patience, cleanup){
+        // Handler
+        File.prototype.$write = function(txt, mode) {
 
-        patience = patience || 60000;
-        var ttdelay = 0;
+                if (this.isOpen) this.write(txt, mode);
+                return (this.$open(mode).write(txt), this.close());
+        }
+        // Handler
+        File.prototype.$read = function() {
 
-        while(1)
-        {       
-                if(this.exists)
-                {
-                        (!cleanup) || (this.remove());
-                        break;
+                if (!this.exists) throw Error("Can't read a non-existent file!");
+                
+                var d = this.$open("r").read();
+                this.$close();
+                return d;
+        }
+        // Handler
+        File.prototype.$clear = function(txt) {
+
+                return (this.$write(txt || ""), this);
+        }
+        // Handler
+        File.prototype.$seek = function(pos) {
+
+                if (!this.isOpen) this.$open('r');
+                return (this.seek(pos), this);
+        }
+        // Handler
+        File.prototype.$create = function(text, encoding) {
+
+                this.encoding = encoding || "UTF-8";
+                return (this.$write((text || ""), 'w'), this);
+        }
+        // Handler
+        File.prototype.$execute = function(slp, cb, doClose) {
+
+                this.execute();
+                if(!!doClose) this.$close();
+                $.sleep(slp || 0);
+                if(typeof callback == "function") cb.call(this);
+
+                return this;
+        }
+        // Info
+        File.prototype.$lines = function() {
+
+                var lines = [];
+                this.$open("r");
+
+                while (!this.eof) lines.push(this.readln());
+
+                return (this.$close(), lines);
+        }
+        // Wait
+        File.prototype.$listenForChange = function(debug, wait, maxiter) {
+
+                var iter = -1, maxiter = maxiter || 100;
+
+                while (++iter < maxiter) {
+                        if (this.modified > lmod) break;
+                        $.$sleep(
+                                !wait? 180: wait == "exp"? Math.round(2, iter+6):
+                                wait,
+                                debug,
+                                iter
+                        );
                 }
-                if(ttdelay > patience) break;
-                $.$sleep(delay, debug, "File not found yet");
-                ttdelay += delay;
+
         }
-}
-File.prototype.getDuration = function(){
-    
-    if(!this.exists) return 0;
-    if(!["video", "audio"].includes(this.getType())) return 0;
-    
-    k = app.project.importFile(new ImportOptions(this));
-    d = k.duration;
-    
-    k.remove(); k = null;
-    return d;
-}
-File.prototype.getName = function(){
-        return this.name.replace(/.[^.]+$/, "");
-}
-File.prototype.getExtension = function(){
-        return this.name.replace(/^.*\./, "");
-}
-File.prototype.getType = function(){
+        // Wait
+        File.prototype.$listenForChar = function(charac, pos, wait, maxiter, debug) {
 
-        xt = this.name.replace(/^.*\./,"").toLowerCase();
-        tp = File.TYPES_BY_EXTENSION[xt] || 7;
-        nm = File.CATEGORIES[tp].toLowerCase();
+                var iter = -1, maxiter = maxiter || 100;
+                while (++iter < maxiter) {
 
-        return nm;
-}
-File.remove = function(){
-      
-        var args = Array.prototype.slice.call(arguments),
-            i    = -1, len = args.length;
-  
-        for(;++i< len;) if(args[i].constructor == File) args[i].remove();
-}
+                        if (this.$open('r').$seek(pos).readch() == charac) break;
+                        else $.$sleep(wait, debug, iter);
+                }
+                
+                this.$close();
+        }
+        File.prototype.$listen = function(delay, debug, patience, cleanup){
 
+                patience = patience || 60000;
+                var ttdelay = 0;
 
-// Folder Handler
-Folder.prototype.$clearFolder = function(extensionName) {
+                while(1)
+                {       
+                        if(this.exists)
+                        {
+                                (!cleanup) || (this.remove());
+                                break;
+                        }
+                        if(ttdelay > patience) break;
+                        $.$sleep(delay, debug, "File not found yet");
+                        ttdelay += delay;
+                }
+        }
+        File.prototype.getDuration = function(){
+        
+                if(!this.exists) return 0;
+                if(!["video", "audio"].includes(this.getType())) return 0;
+                
+                k = app.project.importFile(new ImportOptions(this));
+                d = k.duration;
+                
+                k.remove(); k = null;
+                return d;
+        }
+        File.prototype.getName = function(){
+                return this.name.replace(/.[^.]+$/, "");
+        }
+        File.prototype.getExtension = function(){
+                return this.name.replace(/^.*\./, "");
+        }
+        File.prototype.getType = function(){
 
-        if(this.constructor !== Folder) return;
-        if (this.fsName.checkFF() != -1) throw Error("dirPath is not a folder path");
-        var isAll = (typeof extensionName == "undefined")? true: false;
+                xt = this.name.replace(/^.*\./,"").toLowerCase();
+                tp = File.TYPES_BY_EXTENSION[xt] || 7;
+                nm = File.CATEGORIES[tp].toLowerCase();
 
-        var ffs = this.getFiles();
+                return nm;
+        }
+})();
 
-        ffs.forEach(function(f) {
-                var ext = f.fsName.split('.');
-                ext = ext[ext.length-1];
-                if (f.constructor == File && (isAll || (ext == extensionName)) ) f.remove();
-        })
+(function FolderExtens()
+{
+        // Folder Handler
+        Folder.prototype.$clearFolder = function(extensionName) {
 
-        return 0;
-}
-Folder.prototype.$remove = function(){
-        if(this.constructor !== Folder) return;
-        return (this.$clearFolder(), this.remove(), 0);
-}
-Folder.prototype.getFolders = function(){
+                if(this.constructor !== Folder) return;
+                if (this.fsName.checkFF() != -1) throw Error("dirPath is not a folder path");
+                var isAll = (typeof extensionName == "undefined")? true: false;
 
-      if(this.constructor !== Folder) return;
-      var al = [];
-      this.getFiles().forEach(function(f){ if(f.constructor == Folder) al.push(f)})
-      return al; 
-}
-Folder.prototype.$getFiles = function(){
-      if(this.constructor !== Folder) return;
-      var al = [];
-      this.getFiles().forEach(function(f){ if(f.constructor == File) al.push(f)})
-      return al; 
-}
+                var ffs = this.getFiles();
+
+                ffs.forEach(function(f) {
+                        var ext = f.fsName.split('.');
+                        ext = ext[ext.length-1];
+                        if (f.constructor == File && (isAll || (ext == extensionName)) ) f.remove();
+                })
+
+                return 0;
+        }
+        Folder.prototype.$remove = function(){
+                if(this.constructor !== Folder) return;
+                return (this.$clearFolder(), this.remove(), 0);
+        }
+        Folder.prototype.getFolders = function()
+        {
+
+                if(this.constructor !== Folder) return;
+                var al = [];
+                this.getFiles().forEach(function(f){ if(f.constructor == Folder) al.push(f)})
+                return al; 
+        }
+        Folder.prototype.$getFiles = function(){
+                
+                if(this.constructor !== Folder) return;
+                
+                var al = [];
+                this.getFiles().forEach(function(f){ if(f.constructor == File) al.push(f)})
+                return al; 
+        }
+})();

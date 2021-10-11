71eb5eb3bdba7e81b80e14cc6d5effe73a45c0c5
yahiaboudah <yahiabouda@hotmail.com>
2021-10-11 17:58:52 +0100

    rewrite $HTTP.jsx

--- jsx/$http.jsx
+++ jsx/$http.jsx
@@ -2,10 +2,50 @@
 
 function $http(config)
 {
+	jj =
+	{
+		ser: function(data)
+		{
+			return JSON.stringify(data);
+		},
+		deser: function(data)
+		{
+			return JSON.parse(data);
+		}
+	}
+
+	function makeRequest()
+	{
+		var request = 
+		[
+			"{0} {1} HTTP/1.0".f(method, url.path),
+			"Connection: close",
+			"Host: {0}".f(url.host)
+		
+		].join("\r\n"), header;
+
+		// PÄYLOAD:
+		if(typeof config.payload === "object")
+		{
+			config.payload = jj.ser(config.payload);
+			config.headers = config.headers || {};
+
+			config.headers["Content-Type"]   = "application/json";
+			config.headers["Content-Length"] = config.payload.length;
+		}
+
+		// ADD [HEADER] INFO:
+		for(h in config.headers) request += "\r\n{0}: {1}".f(h, config.headers[h]);
+
+		return request;
+	}
+
     return function(config) {
 
-		var socket      = new Socket(),
-			URL_PATTERN = (/^(.*):\/\/([A-Za-z0-9\-\.]+):?([0-9]+)?(.*)$/);
+		var socket       = new Socket(),
+			URL_PATTERN  = (/^(.*):\/\/([A-Za-z0-9\-\.]+):?([0-9]+)?(.*)$/),
+			HTTP_PATTERN = (/^HTTP\/([\d\.?]+) (\d+) (.*)\r/),
+			HTTP_REGEX   = (/(.*): (.*)\r/g);
 
 		var url    = URL_PATTERN.exec(config.url),
         	method = config.method || 'GET';
@@ -30,48 +70,34 @@ function $http(config)
 		if(!isOpen) throw Error("Can't connect to {0}".f(linkStr)); //|
 		//-------------------------------------------------------------
 
-		var request = 
-		[
-			"{0} {1} HTTP/1.0".f(method, url.path),
-			"Connection: close",
-			"Host: {0}".f(url.host)
 		
-		].join("\r\n");
+		// WRITE THE [REQUEST]:
+		var req = makeRequest();
+		socket.write("{0}\r\n\r\n".f(req));
+		if(config.payload) socket.write(config.payload);
 
-		if(config.payload) 
-        {
-			if(typeof config.payload === 'object') 
-            {
-				config.payload = JSON.stringify(config.payload);
-				(config.headers = config.headers || {})["Content-Type"] = "application/json";
-			}
 
-			(config.headers = config.headers || {})["Content-Length"] = config.payload.length;
+		// [RESPONSE] HANDLING:
+		var payload, http = {};
+		for(var data = socket.read(); !socket.eof;)
+		{
+			data += socket.read();
 		}
-
-		for(header in (config.headers || {})) 
-        {
-			request += "\r\n{0}: {1}".f(header, config.headers[header]);
-		}
-
-		s.write("{0}\r\n\r\n".f(request));
-
-		if(config.payload) s.write(config.payload);
-
-
-		var data, response, payload, http = {};
-		data = s.read();
-		while(!s.eof) data += s.read();
 
 		var response = data.indexOf("\r\n\r\n");
-		if(response == -1) throw "No HTTP payload found in the response!";
 
-		payload  = data.substr(response + 4);
+		//--------------------------------------------------------------------------
+		if(response == -1) throw Error("No HTTP payload found in the response.");//|
+		//--------------------------------------------------------------------------
+
 		response = data.substr(0, response);
+		payload  = data.substr(response + 4); // after response..
 
-		var http = /^HTTP\/([\d\.?]+) (\d+) (.*)\r/.exec(response), header;
+		var http = HTTP_PATTERN.exec(response), header;
 		
-        if(http == null) throw "No HTTP payload found in the response!";
+		//-----------------------------------------------------------------
+        if(!http) throw Error("No HTTP payload found in the response!");//|
+		//-----------------------------------------------------------------
 
 		http = 
         {
@@ -79,21 +105,26 @@ function $http(config)
 			status        : Number(http[2]),
 			statusMessage : http[3],
 			headers       : {}
-		};
+		}
 
-		var httpregex = /(.*): (.*)\r/g;
-
-		while(header = httpregex.exec(response)) http.headers[header[1]] = header[2];
+		while(header = httpregex.exec(response))
+		{
+			http.headers[header[1]] = header[2];
+		}
 
 		var contenttype = (http.headers["Content-Type"] || http.headers["content-type"] || '').split(";");
-		var charset = config.charset || (contenttype[1] ? /charset=(.*)/.exec(contenttype[1])[1] : null);
+		var charset     = config.charset || (contenttype[1] ? /charset=(.*)/.exec(contenttype[1])[1] : null);
 
 		if(charset) payload = payload.toString(charset);
 		contenttype = contenttype[0];
 
-		if(config.forcejson || contenttype == "application/json") http.payload = JSON.parse(payload);
+		if(config.forcejson || contenttype == "application/json")
+		{
+			http.payload = jj.deser(payload);
+		}
+
 		else http.payload = payload;
 		
 		return http;
 	}
-};
+}

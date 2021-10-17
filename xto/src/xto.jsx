3c5f8f72c45c5ba8c781e33fa14e412dbdda0ebd
yahiaboudah <yahiabouda@hotmail.com>
2021-10-17 17:14:49 +0100

    extend CameraLayer with matrix calcs

--- xto/src/xto.jsx
+++ xto/src/xto.jsx
@@ -755,8 +755,46 @@
             {
                 var filmSize    = this.containingComp.height;
                     focalLength = this.getProp("Camera Options/Zoom").value;
-                    
+
                 return MathEx.getAOV(filmSize, focalLength);
+            }
+
+            
+            CameraLayer.prototype.getWorldMatrix = function()
+            {
+                return LayerEx.getWorldMatrix(camera = this);
+            }
+            
+            CameraLayer.prototype.getLocalMatrix = function()
+            {
+                var camera = this;
+                var lookAtMatrix = LayerEx.getLookAt(camera);
+                var localMatrix  = Matrix.multiplyArrayOfMatrices([
+
+                    LayerEx.getRotationMatrix(camera),
+                    LayerEx.getOrientationMatrix(camera),
+                    Matrix.invert(lookAtMatrix),
+                    LayerEx.getPositionMatrix(camera),
+                ]);
+        
+                return localMatrix;
+            }
+
+            CameraLayer.prototype.getProjectedZ = function(w)
+            {
+                var z = this.getProp("Camera Options/Zoom").value;
+                return (z - (z / w));
+            }
+
+            CameraLayer.prototype.getViewMatrix = function()
+            {
+                var viewMatrix;
+
+                return viewMatrix = Matrix.multiplyArrayOfMatrices([
+                    
+                    this.getLocalMatrix(camera),
+                    this.getWorldMatrix(camera),
+                ]);
             }
         }),
 

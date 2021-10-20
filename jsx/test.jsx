2ab3c1493f41a5e90b369dfa4327b16f30b488e6
yahiaboudah <yahiabouda@hotmail.com>
2021-10-20 14:33:05 +0100

    add getType function

--- jsx/test.jsx
+++ jsx/test.jsx
@@ -1,2 +1,20 @@
 
+LayerCollection.prototype.$add = function(what, cfg)
+{
+    switch (what) {
+        case "ShapeLayer":
+            
+            break;
+    
+        default:
+            break;
+    }
+}
 
+
+
+var comp = app.project.activeItem;
+
+$.writeln(comp.layer(1).)
+
+// comp.layers.$add();

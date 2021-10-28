18086e2f1b75e49bac4d860b271ce86c0f169131
yahiaboudah <yahiabouda@hotmail.com>
2021-10-28 19:52:23 +0100

    change Object.keys, remove dontEnumBug

--- jsx/test3.jsx
+++ jsx/test3.jsx
@@ -1,1 +1,5 @@
 
+
+
+$.writeln([1,5,6].propertyIsEnumerable("length"))
+$.writeln([1,5,6].hasOwnProperty("length"))
--- xto/src/xto.jsx
+++ xto/src/xto.jsx
@@ -2567,44 +2567,15 @@
                     return valArr;
                 },
 
-                keys: (function () {
-                    'use strict';
-                    var hasOwnProperty = Object.prototype.hasOwnProperty,
-                        hasDontEnumBug = !({ toString: null }).propertyIsEnumerable('toString'),
-                        dontEnums = [
-                            'toString',
-                            'toLocaleString',
-                            'valueOf',
-                            'hasOwnProperty',
-                            'isPrototypeOf',
-                            'propertyIsEnumerable',
-                            'constructor'
-                        ],
-                        dontEnumsLength = dontEnums.length;
-                
-                    return function (obj) {
-                        if (typeof obj !== 'function' && (typeof obj !== 'object' || obj === null)) {
-                            throw new TypeError('Object.keys called on non-object');
-                        }
-                
-                        var result = [], prop, i;
-                
-                        for (prop in obj) {
-                            if (hasOwnProperty.call(obj, prop)) {
-                                result.push(prop);
-                            }
-                        }
-                
-                        if (hasDontEnumBug) {
-                            for (i = 0; i < dontEnumsLength; i++) {
-                                if (hasOwnProperty.call(obj, dontEnums[i])) {
-                                    result.push(dontEnums[i]);
-                                }
-                            }
-                        }
-                        return result;
-                    };
-                }()),
+                keys: function(obj)
+                {
+                    if(obj.isnt(Object)) throw TypeError("Object.keys called on non-object");
+
+                    var ks = [];
+                    for(prop in obj) if(prop.in(obj)) ks.push(prop);
+
+                    return ks;
+                },
 
                 newKeys: function(obj, keys, values){
                     for(var i=0,len = keys.length; i< len; i++) obj[keys[i]] = values[i];

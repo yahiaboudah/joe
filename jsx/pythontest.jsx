c74c341dd67f1e75093a8190ed0a53a88739b972
yahiaboudah <yahiabouda@hotmail.com>
2021-11-01 16:01:32 +0100

    remove test files

--- jsx/pythontest.jsx
+++
@@ -1,4 +0,0 @@
-//@include "$python.jsx"
-
-zip = python.build("pythonKid");
-$.writeln(zip.add_numbers([1,5,8,88]));
--- jsx/test3.jsx
+++
@@ -1,5 +0,0 @@
-
-
-
-$.writeln([1,5,6].propertyIsEnumerable("length"))
-$.writeln([1,5,6].hasOwnProperty("length"))

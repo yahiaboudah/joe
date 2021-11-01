77d5a0ce3ca116c3d924d73c007328523596d771
yahiaboudah <yahiabouda@hotmail.com>
2021-11-01 11:54:07 +0100

    remove a bunch of files

--- xto/src/xto.jsx
+++ xto/src/xto.jsx
@@ -567,7 +567,7 @@
             
         }),
 
-        MATH$TRIG: (function()
+        MATH$MATH: (function()
         {
             Math.xt({
                 
@@ -579,6 +579,15 @@
                 radiansToDegrees : function(radians)
                 {
                     return radians * 180 / Math.PI;
+                },
+
+                mult : function(){
+                    var args = Array.prototype.slice.call(arguments);
+                    
+                    var i = args.length, mm = 1;
+                    while(i--) mm *= args[i];
+                
+                    return mm
                 }
             })
         }),

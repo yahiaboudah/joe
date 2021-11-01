f4ae9c78e80fa9d79500cfec6cd25ccc4e5e13cc
yahiaboudah <yahiabouda@hotmail.com>
2021-11-01 16:09:14 +0100

    add docs to makeGroupVisible, declutter

--- xto/src/xto.jsx
+++ xto/src/xto.jsx
@@ -3537,7 +3537,7 @@
                 timeme : function(thisArg, args, n)
                 {
                     const MICS = 1000000;
-                    
+
                     var tt = 0,
                         i  = n || 1;
 
@@ -4129,6 +4129,16 @@
         SCUI$DropDownList_prototype: (function(){
             
             DropDownList.prototype.makeGroupVisible = function(g) // g: group var name
+            /**
+             * Changes the visibility of stack-oriented groups' items.
+             * When an item is selected, only the group associated
+             * with the item will be visible. 
+             * 
+             * ==> Before you run this function, make sure you:
+             * 
+             * 1) Associate the item like so: item.group = mygroup1;
+             * 2) Call this like: stdVisibleOnChange("group")
+             */
             {
                 const P = "visibility", N = false, Y = true;
             

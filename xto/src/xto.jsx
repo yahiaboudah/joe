82530e9f0868bca79d858f0aa499079f4964f085
yahiaboudah <yahiabouda@hotmail.com>
2021-11-02 11:10:58 +0100

    add EXPRESSIONS global constant

--- xto/src/xto.jsx
+++ xto/src/xto.jsx
@@ -1782,6 +1782,31 @@
                 }
 
                 return "ADBE";
+            };
+
+            EXPRESSIONS = 
+            {
+                SampleImage: function(layerArg, valArg){
+        
+                    return (function(){
+                    
+                        var targetLayer = thisComp.layer("$layerName");
+                        var compDimens  = [thisComp.width, thisComp.height]; 
+                        
+                        255 * targetLayer.sampleImage(
+            
+                            compDimens/2, //samplePoint
+                            compDimens,   //sampleRadius
+                            true, 
+                            time
+                        
+                        )[$RGBValue].toFixed();
+                    
+                    }).body()._replace({
+                        $layerName: layerArg.name,
+                        $RGBValue: valArg
+                    });
+                }
             }
         }),
 

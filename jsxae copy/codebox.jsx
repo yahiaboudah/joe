022039b9ac7ab0d5fbe93135380d1d45655416be
yahiaboudah <yahiabouda@hotmail.com>
2021-11-05 16:12:45 +0100

    reorganize codebox code into $add functions

--- jsxae copy/codebox.jsx
+++ jsxae copy/codebox.jsx
@@ -1,50 +1,34 @@
 
-function bigbox(c){
+function codebox(c){
 
-  var c = c || app.project.activeItem;
+  c.layers.$add("text", {text: codeStr}, {
+    
+    style: 
+    {
+      size: 45,
+      font: "DejaVuSansMono",
+      color: [0,0,0],
+      position: [ src.left+50,src.top+55 ],
+    },
+    parent: box
+  });
 
-  var defVals = {
-    size: [924, 924],
-    roundness: 45,
-    color: [1,1,1,1]
-  }
+  c.layers.$add("shape", {}, {
 
-  var box = c.layers.addShape(),
-      con = box.property("Contents"), //contents
-      fil = con.addProperty("ADBE Vector Graphic - Fill").property("Color"); //fill
-      rec = con.addProperty("ADBE Vector Shape - Rect"), // rectangle
-      siz = rec.property("Size"), //size
-      rnd = rec.property("ADBE Vector Rect Roundness"), //roundness
-
-  siz.setValue(defVals.size);
-  rnd.setValue(defVals.roundness);
-  fil.setValue(defVals.color);
-  
-  return box;
-}
-
-function outputBox(myCode){
-
-  var c   = app.project.activeItem,
-      out = output(myCode),
-      txt = c.layers.addText(out),
-      box = bigbox(c);
-
-  c.layers.$add("text", {
+    groups: [
+    // GROUPS
+      {
+        name: "rect",
+        children: [
+          {
+            type: "rectangle",
+            name: "myRect1",
+            fill: {color: [1,1,1,1], param: 0}
+          }
+        ]
+      }
+    // GROUPS
+    ]
     
   })
-      
-  src = box.sourceRectAtTime(0,false);
-  box.transform.position.setValue([1350,540]);
-  
-  return txt.config({
-    parent: box,
-    size: 45,
-    font: "DejaVuSansMono",
-    color: [0,0,0],
-    position: [ src.left+50,src.top+55 ],
-  });
 }
-
-// var codeText = codeLayer("shit.py");
-// var outText  = outputLayer(codeText);
---
+++ test2.jsx
@@ -0,0 +1,7 @@
+
+
+oo = {
+    assume: [5, 10]
+}
+
+for(x in oo) $.writeln(oo[x])

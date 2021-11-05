e24e75320a8469487a6fe40d77cdbf7351241860
yahiaboudah <yahiabouda@hotmail.com>
2021-11-05 16:45:52 +0100

    declutter jsxae

--- jsxae copy/explode.jsx
+++ jsxae copy/explode.jsx
@@ -1,91 +1,4 @@
 
-//====================================================================================================================================
-//====================================================================================================================================
-//============================================ PROGRESS BAR ==========================================================================
-function ProgressBar(min, max, current)
-{
-    var _window,
-        _progressBar,
-        _infos,
-        _real,
-        _cursor,
-        _isVisible;
-
-    this.testInfos = 'Processing element $current/$max';
-
-    this.constructor = function(min, max, current)
-    {
-        _this = this;
-        _isVisible = false;
-
-        _real = { min : min, max : max, current : current };
-        _cursor = { min : 0, max : 100, current : 0 };
-
-        _cursor.max = (_real.max - _real.min) + 1;
-
-        // Instanciate the window
-        _window = new Window('palette', "configs.title", undefined, {
-            resizeable : false,
-            borderless : 'not quite true',
-        });
-        _window.preferredSize = [420, 40];
-
-        // Instanciate the progress bar
-        _progressBar = _window.add("progressbar", undefined, _cursor.min, _cursor.max);
-        _progressBar.preferredSize.width = 400;
-        _progressBar.show();
-
-        // Instanciate text infos
-        _infos = _window.add("statictext", undefined, 'Loading, please wait', {
-            justify: 'center'
-        });
-        _infos.preferredSize = [400, 17];
-
-        this.update(current);
-
-
-        return this;
-
-    }
-
-    this.start = function () {
-        _isVisible = true;
-        this.update(_real.current)
-        _window.show();
-    }
-
-    this.end = function () {
-        _window.hide();
-    }
-
-    this.update = function(step) {
-
-        _real.current = step;
-        _cursor.current = (_real.current + 1) - _real.min;
-
-        var infos = this.testInfos._replace({
-            $current: _cursor.current,
-            $max    : _cursor.max
-        });
-
-        _progressBar.value = _cursor.current;
-        _infos.text = infos;
-
-        updateGraphics();
-    }
-
-    function updateGraphics() {
-        if(!_isVisible) return;
-        _window.update();
-    }
-
-    return this.constructor(min, max, current);
-
-}
-
-// ====================================================================================================================================
-// ====================================================================================================================================
-
 MATCH_NAMES = 
 {
     // GRAPHIC PROPERTIES
@@ -126,13 +39,9 @@ function explodeLayer(layer)
     var layers   = [], newLayer,
         allProps = layer.contents.properties();
 
-    var pb = new ProgressBar(1, allProps.length, 1);
-    pb.start();
-
     // Loop through contents:
     allProps.forEach(function(_prop, idx)
     {
-        pb.update(idx);
         if(!_prop.enabled) return;
 
         newLayer = layer.clone({
@@ -151,31 +60,12 @@ function explodeLayer(layer)
         layers.push(newLayer);
     })
 
-    pb.end();
-
     layers.forEach(function(layer){
         layer.enabled = true;
         layer.shy     = false;
     })
 
     return layers;
-}
-
-function explode()
-{
-    // Check if multiple layers selected
-    var comp   = app.project.activeItem,
-        layers = comp.sel(ShapeLayer),
-        layer  = layers[0];
-    
-    if(layer.is(undefined))       throw Error("Select at least 1 Shape Layer!");
-    if(layers[1].isnt(undefined)) throw Error("Select Only 1 Shape Layer!");
-
-    app.wrapUndo(
-
-        ShapeLayer.prototype.explode,
-        layer
-    )();
 }
 
 function cloneLayer(layer) 
@@ -238,7 +128,22 @@ function copyProperties(origin, target)
     w = new Window("palette", "EXPLODE SHAPE!");
     b = w.add("iconbutton", undefined, "/d/media/explode.png");
     b.text = "EXPLODE!";
-    b.onClick = explode;
+    b.onClick = function explode()
+    {
+        // Check if multiple layers selected
+        var comp   = app.project.activeItem,
+            layers = comp.sel(ShapeLayer),
+            layer  = layers[0];
+        
+        if(layer.is(undefined))       throw Error("Select at least 1 Shape Layer!");
+        if(layers[1].isnt(undefined)) throw Error("Select Only 1 Shape Layer!");
+    
+        app.wrapUndo(
+    
+            ShapeLayer.prototype.explode,
+            layer
+        )();
+    };
 
     return w;
 

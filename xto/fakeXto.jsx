56c0bf156a55f24d35a290b83c19ba2ea5e1a537
yahiaboudah <yahiabouda@hotmail.com>
2021-11-08 12:12:43 +0100

    create DEP management structure. Load/isLoaded

--- xto/fakeXto.jsx
+++ xto/fakeXto.jsx
@@ -3,8 +3,42 @@
 
     H[S] = S;
 
+    BASC = (function(){
+
+        Object.prototype.in = function(oo)
+        {
+            var k = this;
+            if(typeof oo !== "array") return;
+
+            for(;++i<oo.length;) if(oo[i] == k) return true;
+
+            return false;
+        }
+    });
+
+    //==================
+    BASC.call($.global);
+    //==================
+
+    S.loaded = [];
+    S.isLoaded = function(what)
+    {
+        return what.in(S.loaded);
+    }
+
     S.load = function(what)
     {
+        // Deal with DEP:
+        var deps = TREE[what].DEPS, i=-1;
+
+        for(;++i<deps.length;)
+        {
+            n = deps[i];
+            if(!(f = FUNS[n]) || S.isLoaded(n)) continue;
+            f.call($.global);
+            S.loaded.push(n);
+        }
+
         FUNS[what].call($.global);
     }
 
@@ -21,22 +55,87 @@
         }
     }
 
-    var TREE = {
-        "myFunction": [
-            "$.global.typeMonkey"
+    var TREE = 
+    {
+        animals:
+        {
+            DEPS: ["monkey", "giraffe", "elephant"],
+            FUNS: [
+                "$.global.callAnimal"
+            ]
+        },
+
+        monkey:[
+            "monkeyName"
+        ],
+
+        giraffe: [
+            "giraffeNoise"
+        ],
+
+        elephant: [
+            "indianElephant"
         ]
     }
 
     var FUNS = 
     {
-        myFunction: (function(){
+        animals: (function(){
+            
+            /**
+             * DEP: [monkey, giraffe, elephant]
+             */
 
-            $.global.typeMonkey = function()
+            $.global.callAnimal = function(name)
             {
-                $.writeln("monkey")
+                switch (name) {
+                    case "monkey":
+                        $.writeln(monkeyName())
+                        break;
+                
+                    case "giraffe":
+                        $.writeln(giraffeNoise());
+                        break;
+
+                    case "elephant":
+                        $.writeln(indianElephant());
+                        break;
+
+                    default:
+                        $.writeln("UNICORN DOESN't EXIST!")
+                        break;
+                }
             }
 
+        }),
+
+        monkey: (function(){
+
+            $.global.monkeyName = function()
+            {
+                return "MONKEY!";
+            }
+
+        }),
+
+        giraffe: (function(){
+            $.global.giraffeNoise = function()
+            {
+                return "GIRAFFE!"
+            }
+        }),
+
+        elephant: (function(){
+
+            $.global.indianElephant = function()
+            {
+                return "INDIAN ELEPHANT!"
+            }
         })
     }
 
 })($.global, {toString: function(){return "fakeXto"}})
+
+fakeXto.load("animals");
+
+callAnimal("elephant");

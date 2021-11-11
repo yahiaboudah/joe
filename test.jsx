5d949c4c7e0a9cb658a9d1d61cefd9fcaaaf1e0e
yahiaboudah <yahiabouda@hotmail.com>
2021-11-11 20:16:11 +0100

    test CSTR$Table

--- test.jsx
+++ test.jsx
@@ -2,8 +2,10 @@
 //@include "xto/src/xto.jsx"
 
 xto.load("CSTR$Python");
+xto.load("CSTR$Table");
 
 $.writeln(Python.testPython());
 
 $.writeln(xto.LOADED.asModule);
 $.writeln(xto.LOADED.asDepend.$$$$$MISC);
+$.writeln(xto.LOADED.asDepend.String); 

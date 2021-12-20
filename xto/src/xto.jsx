0e8080c39de36543aad8720b5190ca45f2aaadfd
yahiaboudah <yahiabouda@hotmail.com>
2021-12-21 00:22:37 +0100

    test HTTP class, works (Forbidden status message)

--- xto/src/xto.jsx
+++ xto/src/xto.jsx
@@ -848,9 +848,6 @@
             // Deal with DEP:
             // First preprocess the name:
             var pWhat = what.split('/'), eWhat = EXTO, k =-1;
-
-            $.writeln(pWhat.length);
-
             while(++k<pWhat.length) eWhat = eWhat[pWhat[k]];
 
             var deps = eWhat? eWhat.DEPS:{}, i=-1;
@@ -1729,16 +1726,15 @@
 
         $$$$$DATA: (function(){
 
-            /*
             $.global.HTTP = function HTTP(url)
             {
-                this.S = new Socket(), L;
+                this.S = new Socket();
                 
-                var url = HTTP.PATTERNS.URL.exec(url);
+                var url = HTTP.PATTERNS.URL.exec(url), L;
                 
                 if(url === null)     throw Error("HTTP: Invalid URL");
                 if(url[1] != "http") throw Error("HTTP: Invalid Scheme");
-
+            
                 this.U = 
                 {
                     scheme: url[1],
@@ -1746,7 +1742,7 @@
                     port  : url[3] || 80,
                     path  : url[4]
                 }
-
+            
                 if(!this.S.open(L = "{0}:{1}".re(this.U.host, this.U.port), "binary"))
                 {
                     throw Error("HTTP: Failed to Connect to {0}".re(L));
@@ -1755,11 +1751,11 @@
             
             // [PATTERNS]
             HTTP.xt({
-
+            
                 PATTERNS:
                 {
                     // Use brackets to extract groups later with patt.exec(str)
-
+            
                     URL  : new RegExp(
                         [
                             "^(.*):\/\/",         // => anything://
@@ -1772,11 +1768,11 @@
                     
                     HTTP : new RegExp(
                         [
-                            "/^HTTP\/",     // => HTTP/
-                            "[\d\.?]+",     // => 88.
-                            "\d+",          // => 781
-                            ".*\r/",        // => knaze    
-                        ].join('')          // HTTP/88.781knaze
+                            "^HTTP\/",      // => HTTP/
+                            "([0-9]\.[0-9]) ", // => 1.1
+                            "([0-9]+) ",       // => 403
+                            "(.*)",        // =>  Forbidden    
+                        ].join('')          // HTTP/1.1 403 Forbidden
                     ),
                     
                     HTTP_HEADER : new RegExp(
@@ -1788,32 +1784,33 @@
                     )
                 }
             })
-
+            
             // [REQUST MAKING]
             HTTP.prototype.xt({
-
-                request: function(M, config)
+            
+                request: function(M/*ethod*/, config)
                 {
                     // PAYLOAD & HEADERS (default/serialization):
+                    config = config || {};
                     config.payload = $.ser(config.payload || {});
                     config.headers = config.headers || {};
-                    config.connectionType = config.connectionType || close;
-
+                    config.connectionType = config.connectionType || "close";
+            
                     // BASIC REQUEST STRUCTURE
                     var R =
                     [
                         "{0} {1} HTTP/1.0",
                         "Host: {2}",
                         "Connection: {3}"
-
+            
                     ].join("\r\n").re(M, this.U.path, this.U.host, config.connectionType), h, H;
-
+            
                     // Default headers
                     H = config.headers.xt({
                         "Content-Type"  : "application/json",
                         "Content-Length": config.payload.length 
                     })
-
+            
                     // Insert all headers into Request
                     for(h in H) if(h.in(H)) R += "\r\n{0}: {1}".re(h, H[h]);
                 
@@ -1822,24 +1819,31 @@
                     //=======================================================
                 }
             })
-
+            
             // [RESPONSE HANDLING]
             HTTP.prototype.xt({
-                
-                handleResponse: function(config)
+            
+                rawResponse: function()
                 {
+                    // Socket.read(), read all data
+                    for(var data = this.S.read(); !this.S.eof; data += this.S.read());
+                    return data;
+                },
+                
+                response: function(config)
+                {
+                    config = config || {};
+            
                     var data,
-                        payload, payloadIndex, response, 
+                        payload, payloadIndex, response,
                         http, header;
                     
-                    // Socket.read(), read all data
-                    for(data = this.S.read(); !this.S.eof; data += this.S.read());
-
+                    data = this.rawResponse();
                     payloadIndex = data.indexOf("\r\n\r\n");
                     //--------------------------------------------------------------------------
                     if(payloadIndex == -1) throw Error("HTTP: No Payload found in Response.");//|
                     //--------------------------------------------------------------------------
-
+            
                     response = data.substr(0, payloadIndex);
                     payload  = data.substr(payloadIndex + 4); // after response..
                 
@@ -1861,7 +1865,7 @@
                     {
                         http.headers[header[1]] = header[2];
                     }
-
+            
                     // Deal with ContentType and Charset
                     var contentType = (http.headers["Content-Type"] || http.headers["content-type"] || '').split(";");
                     var charset     = config.charset || 
@@ -1869,13 +1873,12 @@
                     
                     if(charset) payload = payload.toString(charset);
                     contentType = contentType[0];
-                    if(config.forcejson || contenttype == "application/json") http.payload = $.deser(payload);
+                    if(config.forcejson || contentType == "application/json") http.payload = $.deser(payload);
                     
                     return http;
                 }
             })
-
-            */
+            
 
             // [JSON]
             $.json = (function()
@@ -1924,12 +1927,12 @@
                     var partial;
                     var value = holder[key];
             
-                    if(value.is(Object) && value.toJSON.is(Function))
+                    if(is(value, Object) && is(value.toJSON, Function))
                     {
                         value = value.toJSON(key);
                     }
 
-                    if(rep.is(Function)) value = rep.call(holder, key, value);
+                    if(is(rep, Function)) value = rep.call(holder, key, value);
             
                     switch(typeof value)
                     {
@@ -1965,9 +1968,9 @@
                                 return v;
                             }
             
-                            if(rep.is(Object))
+                            if(rep && is(rep, Object))
                             {
-                                length = rep.length, i = -1;
+                                length = rep.length >>> 0, i = -1;
                                 for (;++i<length;)
                                 {
                                     if(rep[i].isnt(String)) continue;

a5b2bad919536d4318f96ae1c0d4caa997041f1e
yahiaboudah <yahiabouda@hotmail.com>
2021-12-21 00:25:13 +0100

    testing http on jsonplaceholder (works perfectly well!!!)

--- test.jsx
+++ test.jsx
@@ -4,10 +4,11 @@ xto.load("$$$$/DATA");
 
 try
 {
-    var hh = new HTTP("http://uniquefreshtranscendentsecret.neverssl.com/online");
+    var hh = new HTTP("http://jsonplaceholder.typicode.com/todos/25");
     hh.request("GET");
     var rr = hh.response();
     $.writeln(rr.statusMessage);
+    $.writeln(rr.payload);
 }
 
 catch(e) {$.writeln(e.line)}

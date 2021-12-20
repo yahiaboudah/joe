35d0808b5dc5e55b1f0690196d9b782bd62e4e04
yahiaboudah <yahiabouda@hotmail.com>
2021-12-20 20:44:02 +0100

    reorganize patterns and add regex comments

--- xto/src/xto.jsx
+++ xto/src/xto.jsx
@@ -1726,7 +1726,161 @@
         */
 
         $$$$$DATA: (function(){
+            
+            // [HTTP]
+            $.xt({
+                
+                http: function(config)
+                {
+                    /*
+                    config = {
+                        url : "http://www.youtube.com",
+                        method: "get"
+                    }
+                    */
 
+                    var PATTERNS = 
+                    {
+                        URL  : new RegExp(
+                            [
+                                "^(.*):\/\/",         // => anything://
+                                "([A-Za-z0-9\-\.]+)", // => www.mywebsite.com
+                                ":?([0-9]+)?",        // => :8080 (optional)
+                                "(.*)$"               // => knazekn (anything at the end)
+                            
+                            ].join('')                // => anything://www.mywebsite.com:8080knazekn
+                        ),
+                        
+                        HTTP : new RegExp(
+                            [
+                                "/^HTTP\/",     // => HTTP/
+                                "[\d\.?]+",     // => 88.
+                                "\d+",          // => 781
+                                ".*\r/",        // => knaze    
+                            ].join('')          // HTTP/88.781knaze
+                        ),
+                        
+                        HTTP_HEADER : new RegExp(
+                            [
+                                "(.*):",    // => khazekhaze:
+                                "(.*)\r"    // => oihairhknq
+                            ].join(''),
+                            'g'
+                        )
+                    }
+
+                    function makeRequest($method, $url)
+                    {
+                        var request = 
+                        [
+                            "{0} {1} HTTP/1.0".re($method, $url.path),
+                            "Connection: close",
+                            "Host: {0}".re($url.host)
+                        
+                        ].join("\r\n"), header;
+                
+                        // PÄYLOAD:
+                        config.payload || (config.payload = {});
+                        if(typeof config.payload === "object")
+                        {
+                            config.payload = $.ser(config.payload);
+                            config.headers = config.headers || {};
+                
+                            config.headers["Content-Type"]   = "application/json";
+                            config.headers["Content-Length"] = config.payload.length;
+                        }
+                
+                        // ADD [HEADER] INFO:
+                        for(h in config.headers) request += "\r\n{0}: {1}".f(h, config.headers[h]);
+                        return request;
+                    }
+
+                    var socket = new Socket();
+                    var url    = PATTERNS.URL.exec(config.url),
+                        method = config.method || 'GET';
+                
+                    //----------------------------------------------------------
+                    if(!url)			 throw Error("UNABLE to parse URL"); //|
+                    if(url[1] != "http") throw Error("ONLY scheme is HTTP"); //|
+                    //----------------------------------------------------------
+                
+                    url =
+                    {
+                        scheme: 
+                        url[1],
+                        host  : url[2],
+                        port  : url[3] || (url[1] == "https" ? 443 : 80),
+                        path  : url[4]
+                    }
+                
+                    var linkStr = "{0}:{1}".f(url.host, url.port),
+                        isOpen  = socket.open(linkStr, "binary");
+                    
+                    //-------------------------------------------------------------
+                    if(!isOpen) throw Error("Can't connect to {0}".f(linkStr)); //|
+                    //-------------------------------------------------------------
+                
+                    
+                    // WRITE THE [REQUEST]:
+                    var req = makeRequest(method, url);
+                
+                    socket.write("{0}\r\n\r\n".re(req));
+                    if(config.payload) socket.write(config.payload);
+                
+                
+                    // [RESPONSE] HANDLING:
+                    var payload, http = {};
+                    for(var data = socket.read(); !socket.eof;)
+                    {
+                        data += socket.read();
+                    }
+                
+                    var response = data.indexOf("\r\n\r\n");
+                
+                    //--------------------------------------------------------------------------
+                    if(response == -1) throw Error("No HTTP payload found in the response.");//|
+                    //--------------------------------------------------------------------------
+                
+                    response = data.substr(0, response);
+                    payload  = data.substr(response + 4); // after response..
+                
+                    var http = HTTP_PATTERN.exec(response), header;
+                    
+                    //-----------------------------------------------------------------
+                    if(!http) throw Error("No HTTP payload found in the response!");//|
+                    //-----------------------------------------------------------------
+                
+                    http = 
+                    {
+                        ver           : Number(http[1]),
+                        status        : Number(http[2]),
+                        statusMessage : http[3],
+                        headers       : {}
+                    }
+                
+                    while(header = HTTP_REGEX.exec(response))
+                    {
+                        http.headers[header[1]] = header[2];
+                    }
+                
+                    var contenttype = (http.headers["Content-Type"] || http.headers["content-type"] || '').split(";");
+                    var charset     = config.charset || (contenttype[1] ? /charset=(.*)/.exec(contenttype[1])[1] : null);
+                
+                    if(charset) payload = payload.toString(charset);
+                    contenttype = contenttype[0];
+                
+                    if(config.forcejson || contenttype == "application/json")
+                    {
+                        http.payload = $.deser(payload);
+                    }
+                
+                    else http.payload = payload;
+                    
+                    return http;
+                }
+            })
+
+            // [JSON]
             $.json = (function()
             {
                 var JJ = {};
@@ -1946,174 +2100,16 @@
             
                 return JJ;
             })();
+            $.ser   = function(data) {return $.json.stringify(data);}
+            $.deser = function(data) {return $.json.parse(data);}
 
+            // [CLIPBOARD]
             $.xt({
-                
+
                 clipboardLibFile: false,
                 clipboardLib : 0,
 
-                ser: function(data)
-                {
-                    return $.json.stringify(data);
-                },
-
-                deser: function(data)
-                {
-                    return $.json.parse(data);
-                },
-
-                http: function(config)
-                {
-                    function makeRequest($method, $url)
-                    {
-                        var request = 
-                        [
-                            "{0} {1} HTTP/1.0".re($method, $url.path),
-                            "Connection: close",
-                            "Host: {0}".re($url.host)
-                        
-                        ].join("\r\n"), header;
                 
-                        // PÄYLOAD:
-                        config.payload || (config.payload = {});
-                        if(typeof config.payload === "object")
-                        {
-                            config.payload = $.ser(config.payload);
-                            config.headers = config.headers || {};
-                
-                            config.headers["Content-Type"]   = "application/json";
-                            config.headers["Content-Length"] = config.payload.length;
-                        }
-                
-                        // ADD [HEADER] INFO:
-                        for(h in config.headers) request += "\r\n{0}: {1}".f(h, config.headers[h]);
-                        return request;
-                    }
-                
-                
-                    var socket       = new Socket(),
-                        
-                        URL_PATTERN  = new RegExp(
-                            [
-                                "^(.*):\/\/",
-                                "([A-Za-z0-9\-\.]+)",
-                                ":?([0-9]+)?",
-                                "(.*)$"
-                            ].join('')
-                        ),
-                        
-                        HTTP_PATTERN = new RegExp(
-                            [
-                                "/^HTTP\/",
-                                "[\d\.?]+",
-                                "\d+",
-                                ".*\r/",
-                            ].join('')
-                        ),
-                        
-                        HTTP_REGEX   = new RegExp(
-                            [
-                                "(.*):",
-                                "(.*)\r"
-
-                            ].join(''),
-                            'g'
-                        );
-                
-                    var url    = URL_PATTERN.exec(config.url),
-                        method = config.method || 'GET';
-                
-                    //----------------------------------------------------------
-                    if(!url)			 throw Error("UNABLE to parse URL"); //|
-                    if(url[1] != "http") throw Error("ONLY scheme is HTTP"); //|
-                    //----------------------------------------------------------
-                
-                    url =
-                    {
-                        scheme: 
-                        url[1],
-                        host  : url[2],
-                        port  : url[3] || (url[1] == "https" ? 443 : 80),
-                        path  : url[4]
-                    }
-                
-                    var linkStr = "{0}:{1}".f(url.host, url.port),
-                        isOpen  = socket.open(linkStr, "binary");
-                    
-                    //-------------------------------------------------------------
-                    if(!isOpen) throw Error("Can't connect to {0}".f(linkStr)); //|
-                    //-------------------------------------------------------------
-                
-                    
-                    // WRITE THE [REQUEST]:
-                    var req = makeRequest(method, url);
-                
-                    socket.write("{0}\r\n\r\n".re(req));
-                    if(config.payload) socket.write(config.payload);
-                
-                
-                    // [RESPONSE] HANDLING:
-                    var payload, http = {};
-                    for(var data = socket.read(); !socket.eof;)
-                    {
-                        data += socket.read();
-                    }
-                
-                    var response = data.indexOf("\r\n\r\n");
-                
-                    //--------------------------------------------------------------------------
-                    if(response == -1) throw Error("No HTTP payload found in the response.");//|
-                    //--------------------------------------------------------------------------
-                
-                    response = data.substr(0, response);
-                    payload  = data.substr(response + 4); // after response..
-                
-                    var http = HTTP_PATTERN.exec(response), header;
-                    
-                    //-----------------------------------------------------------------
-                    if(!http) throw Error("No HTTP payload found in the response!");//|
-                    //-----------------------------------------------------------------
-                
-                    http = 
-                    {
-                        ver           : Number(http[1]),
-                        status        : Number(http[2]),
-                        statusMessage : http[3],
-                        headers       : {}
-                    }
-                
-                    while(header = HTTP_REGEX.exec(response))
-                    {
-                        http.headers[header[1]] = header[2];
-                    }
-                
-                    var contenttype = (http.headers["Content-Type"] || http.headers["content-type"] || '').split(";");
-                    var charset     = config.charset || (contenttype[1] ? /charset=(.*)/.exec(contenttype[1])[1] : null);
-                
-                    if(charset) payload = payload.toString(charset);
-                    contenttype = contenttype[0];
-                
-                    if(config.forcejson || contenttype == "application/json")
-                    {
-                        http.payload = $.deser(payload);
-                    }
-                
-                    else http.payload = payload;
-                    
-                    return http;
-                },
-
-                wget: function(fp, link)
-                {   // get images from the web with cmd utility: [WGET]
-
-                    system.callSystem("cd {0} & wget -O {1} {2}".re(
-                            
-                            Folder(File(fp).path).fsName.replace(/\\/gi, '/'),
-                            fp.replace(/\\/gi, '/'),
-                            link
-                    ));
-                },
-
                 chkClipboard: function(p)
                 {
                     if(!$.clipboardLibFile)
@@ -2139,6 +2135,21 @@
 
                     $.chkClipboard(path);
                     return (new ExternalObject("lib:" + path)).setClipboard();
+                }
+            })
+
+            // [CMD]
+            $.xt({
+
+                wget: function(fp, link)
+                {   // get images from the web with cmd utility: [WGET]
+
+                    system.callSystem("cd {0} & wget -O {1} {2}".re(
+                            
+                            Folder(File(fp).path).fsName.replace(/\\/gi, '/'),
+                            fp.replace(/\\/gi, '/'),
+                            link
+                    ));
                 }
             })
         }),

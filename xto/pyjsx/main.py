157042eafd6cde30ac5e3ccd79c66300adca5198
yahia boudah <yahiabouda@hotmail.com>
2022-01-03 20:18:46 +0100

    break down exec into 4 PYJSX files

---
+++ xto/pyjsx/main.py
@@ -0,0 +1,14 @@
+
+def run():
+    
+    intff['info']['reqs_exec'] = intff['info']['reqs_exec'] + 1
+    intff['active_req']['crop'] = result
+    
+    # dump new interface with crop result
+    with open(intf_path, 'w', encoding='utf8') as f:
+        f.write(json.dumps(intff, indent =4))
+
+    # write signal file
+    with open(exec_signal, 'w') as ef: ef.write('')
+
+if __name__ == "__main__": run()

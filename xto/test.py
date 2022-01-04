45699a36e572d877873b1ed3e84986221a921900
yahia boudah <yahiabouda@hotmail.com>
2022-01-04 22:56:56 +0100

    update test, and pyes

--- xto/test.py
+++ xto/test.py
@@ -1,16 +1,5 @@
-
 from pyes.pyjsx import *
 from pyes.file_interface import *
 
-# ff = FileInterface("C:/Users/bouda/Desktop/myInterface.json")
-# ww = ff.value
-# print(ww)
-
 pp = "C:/Users/bouda/Desktop/extra/extraSon/myInterface2.json"
 ff = FileInterface(intf_path = pp)
-
-print(type([4,"aze", {"ww": "knaze"}]))
-
-# pp = "C:/Users/bouda/Desktop/someshit.txt"
-# print(Utils.file_name(pp))
-# print(Utils.get_user())

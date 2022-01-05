03f0dfceeb47e8b913ce3e69f0db3c11438da173
yahia boudah <yahiabouda@hotmail.com>
2022-01-05 18:29:51 +0100

    add upgrade/setup.cfg, automate PYES version upgrade

---
+++ xto/upgrade.py
@@ -0,0 +1,66 @@
+
+'''
+Update version number and push to PyPi
+'''
+from runpy import run_path
+from collections import OrderedDict
+import subprocess as cmd
+
+class PYJSX_Upgrader():
+
+    _PP = 'C:/oi/joe/xto/pyes/'
+    _VP = "{parent}/pyes/_version.py".format(parent= _PP)
+    _version = run_path(_VP)['__version__']
+
+    @classmethod
+    def upgrade_version(self):
+
+        ver = list((int(v) for v in self._version.split('.')))
+
+        # c < 10
+        if(ver[2]+1 < 10): ver[2] = ver[2]+1
+        # c == 10
+        elif(ver[2]+1 ==10): 
+            ver[2] = 0
+            ver[1] = ver[1]+1
+
+        if(ver[1] ==10):
+            ver[2] = ver[1] = 0
+            ver[0] = ver[0]+1
+
+        self._version = '.'.join(str(v) for v in ver)
+
+    @classmethod
+    def modify_file(self):
+        with open(self._VP, 'w') as f:
+            f.write("__version__ = \'{ver}\'".format(ver=self._version))
+    
+    @classmethod
+    def upgrade(self):
+        self.upgrade_version()
+        self.modify_file()
+
+    @classmethod
+    def run_commands(self, timeout = 9):
+
+        commands = OrderedDict({
+            #pacakge and ship
+            "dist": 'python setup.py sdist',
+            "pypi": 'twine upload dist/* --config-file twine.pypirc',
+            #clean
+            "rmds": 'rmdir dist /s /q',
+            "rmin": 'rmdir pyjsx.egg-info /s /q',
+            #sleep
+            'wait': 'timeout {timeout}'.format(timeout = timeout),
+            #reinstall
+            # 'fpip': 'pip install --no-cache-dir --upgrade -vvv pyjsx=={v}'.format(v= self._version),
+            # 'spip': 'pip show pyjsx'
+        })
+        cfg = {'shell': True, 'cwd': self._PP}
+
+        for c in commands.values(): 
+            cmd.call(c, **cfg)
+
+if __name__ == '__main__':
+    PYJSX_Upgrader.upgrade()
+    PYJSX_Upgrader.run_commands()

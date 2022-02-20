4740ec991c6fa28283a7241738241173fe34f71b
yahia boudah <yahiabouda@hotmail.com>
2022-02-20 15:29:44 +0100

    update TextLayer/ShapeLayer

--- src/AFFX/ShapeLayer.jsx
+++ src/AFFX/ShapeLayer.jsx
@@ -1,298 +1,305 @@
-// [SETTERS/ MODIFIERS]
-ShapeLayer.prototype.xt({
-    
-    /**
-        * effect:Axis
-        * content:group
-    */
-    add: function(what, cfg)
-    {
-        what  = what.split(':');
-        var T = what[0], P = what[1]; //Type & Property
 
-        var matchNames = 
+ShapeLayer
+
+    [STATIC]
+    ({  
+        __name__: "PPS",  
+
+        PROPS:
         {
-            group : "ADBE Vector Group",
-            stroke: "ADBE Vector Graphic - Stroke",
-            strokeWidth: "ADBE Vector Stroke Width",
-            fill: "ADBE Vector Graphic - Fill",
-            fillColor: "ADBE Vector Fill Color",
+            stroke    : ["composite", "color", "strokeWidth", "lineCap", "lineJoin", "miterLimit"],
+            fill      : ["composite", "fillRule", "color"],
+            transform : ["anchorPoint", "position", "scale", "skew", "skewAxis", "rotation", "opacity"],
+            rect      : ["shapeDirection", "size", "position", "roundness"],
+            ellipse   : ["shapeDirection", "size", "position"],
+            star      : ["shapeDirection, type", "points", "position", "rotation", 
+                        "innerRadius", "outerRadius", "innerRoundness", "outerRoundness"]    
         }
+    })
 
-        var S = this,
-            G = S.property(T == "effect"? "Effects": "Contents");
-            G = G.canAddProperty(P)? G.addProperty(P): // can add P? add it
-                G.canAddProperty(matchNames[P])?G.addProperty(matchNames[P]): // try calling the matchName
-                undefined; // otherwise G is undefined
+    [PROTO]
+    ({    
+        /**
+            * effect:Axis
+            * content:group
+        */
         
-        if(!G) return G;
+        __name__: "SETTERS",
 
-        if(cfg)
+        add: function(what, cfg)
         {
-            if(is(cfg.name, String)) G.name = cfg.name;
+            what  = what.split(':');
+            var T = what[0], P = what[1]; //Type & Property
 
-            if(is(cfg.stroke, Number))
+            var matchNames = 
             {
-                G.addProperty(matchNames["stroke"]).property(matchNames["strokeWidth"]).setValue(cfg.stroke);
+                group : "ADBE Vector Group",
+                stroke: "ADBE Vector Graphic - Stroke",
+                strokeWidth: "ADBE Vector Stroke Width",
+                fill: "ADBE Vector Graphic - Fill",
+                fillColor: "ADBE Vector Fill Color",
             }
 
-            if(is(cfg.fillColor, Array))
-            {
-                G.property("Color").setValue(cfg.fillColor);
-            }
-        }
-
-        return G;
-    },
-    
-    get: function(matchName, all)
-    {
-        var L = this, i=0, PP = [];
-        while(++i<L.content.numProperties+1)
-        {
-            if(L.content.property(i).matchName == matchName)
-            {
-                PP.push(L.content.property(i));
-            }
-        }
-
-        return all?PP:PP[0];
-    },
-    
-    moveFirstVertex : function(idx){
-    
-        var i = 0,
-            c = this.property("Contents"),
-            n = c.numProperties + 1;
-    
-        while(++i<n) c.property(i).moveFirstVertex(idx);
-    }
-})
-
-// [INFO / GETTERS]
-ShapeLayer.prototype.xt({
-
-    numProp: function(propName){
-
-        const propTypes = {
-            "Graphic": 
-            {
-                Stroke: "Stroke",
-                GradientStroke: "G-Stroke",
-                Fill: "Fill",
-                GradientFill : "G-Fill"
-            },
+            var S = this,
+                G = S.property(T == "effect"? "Effects": "Contents");
+                G = G.canAddProperty(P)? G.addProperty(P): // can add P? add it
+                    G.canAddProperty(matchNames[P])?G.addProperty(matchNames[P]): // try calling the matchName
+                    undefined; // otherwise G is undefined
             
-            "Filter": 
+            if(!G) return G;
+
+            if(cfg)
             {
-                MergePaths: "Merge",
-                OffsetPaths: "Offset",
-                PuckerAndBloat: "PB",
-                RoundCorners: "RC",
-                TrimPaths: "Trim",
-                TwistPaths: "Twist",
-                WigglePaths: "Roughen",
-                WiggleTransform: "Wiggler",
-                ZigZag: "ZigZag",
-            },
-        
-            "Shape": 
-            {
-                PolyStar: "Star",
-                Rectangle: "Rect",
-                Ellipse: "Ellipse",
-                Custom: "Group"
-            }
-        }
+                if(is(cfg.name, String)) G.name = cfg.name;
 
-        if(_in(propName, ["Shape", "Filter", "Graphic", "Group"]))
-        {
-            propName = "ADBE Vector {0}{1}".re(propName, propName != "Group"?" - .*":"");
-        }
-    
-        var x;
-        for(propType in propTypes) if(propType.in(propTypes))
-        {
-            if(!!(x = propType[propName])) propName = "ADBE Vector {0} - {1}".re(propType, x); 
-        }
+                if(is(cfg.stroke, Number))
+                {
+                    G.addProperty(matchNames["stroke"]).property(matchNames["strokeWidth"]).setValue(cfg.stroke);
+                }
 
-        propName = new RegExp(propName || "ADBE Vector .*", 'g');
-        var c = this.property("Contents"), i = 0, k = 0;
-        while(++ i<c.numProperties+1) if(propName.test(c.property(i).matchName)) k++;
-    
-        return k;
-    },
-
-    grabProps : function()
-    // layer.grabProps("Group", "Shape"); => Group 1, Rectangle 1
-    {
-
-        var _TYPES = 
-        [
-            "Group",
-            "Shape",
-            "Graphic",
-            "Filter"
-        ];
-    
-        var T = arguments.slice(), A= [], i=-1;
-        var C = this.content;
-
-        while(++i<C.numProperties+1){
-            prop = C.property(i);
-            mn = prop.matchName.split('-')[0].trim().split(" ").pop();
-            if(types.includes(mn)) allProps.push(prop);
-        }
-    
-        return A;
-    },
-
-    area : function(t){
-
-        c = this.containingComp;
-        t = t || c.time;
-    
-        sr = this.sourceRectAtTime(t, false); //sourceRect
-        sc = this.transform.scale.value; //scale
-        
-        return Math.mult.apply(null,
-            ([sr.width, sr.height] ^ (sc/100))
-        );
-    },
-
-    alpha : function(t){
-    
-        c = this.containingComp;
-        t = t || c.time;
-
-        var sr = this.sourceRectAtTime(t, false),
-        sc = this.transform.scale;
-
-        var wh = (sc/100) * ([sr.width, sr.height] /2)
-
-        p = [
-            c.toWorld(this, [sr.left, 0])[0], 
-            c.toWorld(this, [0, sr.top])[1]
-        ] + wh;
-        
-        this.addProp("Effects/Color Control:cc").property("Color").expression = (function()
-        {
-            thisLayer.sampleImage($p,$wh)
-        }).body({$p: p, $wh:wh});
-        
-        var rgba = this.getProp("Effects/cc").property("Color").value;
-        this.removeProp("Effects/cc");
-        return rgba[3];
-    },
-
-    areas : function(roundit){
-        
-        var areas     = [],
-            isEnabled = [],
-            contents  = this.property("Contents"),
-            numProps  = contents.numProperties,
-            i         = 0;
-    
-        while(++i < numProps+1)
-        {
-            isEnabled.push(contents.property(i).enabled);
-            contents.property(i).enabled = false;
-        }   
-        
-        i = 0;
-        while (++i< numProps+1)
-        {
-            if(i == 1)
-            {
-                contents.property(i).enabled = true;
-                currArea = this.area() * this.alpha();
-                areas.push(roundit?Math.round(currArea):currArea);
-                continue;
+                if(is(cfg.fillColor, Array))
+                {
+                    G.property("Color").setValue(cfg.fillColor);
+                }
             }
 
-            contents.property(i-1).enabled = false;
-            contents.property(i+0).enabled = true;
+            return G;
+        },
 
-            currArea = this.area() * this.alpha();
-            areas.push(roundit?Math.round(currArea): currArea);
-        }   i = 0;
-    
-        while(++i < numProps+1)
-        {
-            contents.property(i).enabled = isEnabled[i-1];
+        moveFirstVertex : function(idx){
+        
+            var i = 0,
+                c = this.property("Contents"),
+                n = c.numProperties + 1;
+        
+            while(++i<n) c.property(i).moveFirstVertex(idx);
         }
-    
-        return areas;
-    },
+    })
 
-    distances : function(origin)
-    {
-    
-        prop = function(c, n, pp)
+    [PROTO]
+    ({
+        __name__: "GETTERS",
+
+        get: function(matchName, all)
         {
-            return c.property(n)
-                    .property("Transform")
-                    .property(pp).value;
-        }
-        num = function(v){
-            return new Number(v);
-        }
-    
-        funcs = {
-    
-            topleft: function(pos){
-                return Math.sqrt(
-                                (num(pos[0] - src.left) ^ 2) 
-                                + (num(pos[1] - src.top ) ^ 2)
-                        );
-            },
-            left: function(pos){
-                return pos[0] - src.left;
-            },
-            right: function(pos){
-                return wd - (pos[0] - src.left);
-            },
-            top: function(pos){
-                return pos[1] - src.top;
-            },
-            bottom: function(pos){
-                return ht - (pos[1] - src.top);
-            },
-            custom: function(pos){
-                // TODO: Figure out the position coordinates of the
-                // elements relative to the comp coordinate system.
+            var L = this, i=0, PP = [];
+            while(++i<L.content.numProperties+1)
+            {
+                if(L.content.property(i).matchName == matchName)
+                {
+                    PP.push(L.content.property(i));
+                }
+            }
+
+            return all?PP:PP[0];
+        },
+
+        numProp: function(propName){
+
+            const propTypes = {
+                "Graphic": 
+                {
+                    Stroke: "Stroke",
+                    GradientStroke: "G-Stroke",
+                    Fill: "Fill",
+                    GradientFill : "G-Fill"
+                },
                 
-                // Then simply subtract the distances to figure which
-                // ones are closer than others.
-            }
-        }
-    
-        var positions  = [],
-            origin     = origin || "topleft";
-            contents   = this.property("Contents"),
-            numProps   = contents.numProperties;
+                "Filter": 
+                {
+                    MergePaths: "Merge",
+                    OffsetPaths: "Offset",
+                    PuckerAndBloat: "PB",
+                    RoundCorners: "RC",
+                    TrimPaths: "Trim",
+                    TwistPaths: "Twist",
+                    WigglePaths: "Roughen",
+                    WiggleTransform: "Wiggler",
+                    ZigZag: "ZigZag",
+                },
             
-        var src = this.sourceRectAtTime(this.containingComp.time, 0);
-            wd  = src.width;
-            ht  = src.height;
-    
-        for(i=0;++i<numProps+1;) positions.push(prop(contents,i, "Position"))
-    
-        return positions.map(funcs[origin]);    
-    }
-});
+                "Shape": 
+                {
+                    PolyStar: "Star",
+                    Rectangle: "Rect",
+                    Ellipse: "Ellipse",
+                    Custom: "Group"
+                }
+            }
 
-// PROPS:
-ShapeLayer.xt({
-    
-    PROPS :
-    {
-        stroke    : ["composite", "color", "strokeWidth", "lineCap", "lineJoin", "miterLimit"],
-        fill      : ["composite", "fillRule", "color"],
-        transform : ["anchorPoint", "position", "scale", "skew", "skewAxis", "rotation", "opacity"],
-        rect      : ["shapeDirection", "size", "position", "roundness"],
-        ellipse   : ["shapeDirection", "size", "position"],
-        star      : ["shapeDirection, type", "points", "position", "rotation", 
-                    "innerRadius", "outerRadius", "innerRoundness", "outerRoundness"]    
-    }
-})
+            if(_in(propName, ["Shape", "Filter", "Graphic", "Group"]))
+            {
+                propName = "ADBE Vector {0}{1}".re(propName, propName != "Group"?" - .*":"");
+            }
+        
+            var x;
+            for(propType in propTypes) if(propType.in(propTypes))
+            {
+                if(!!(x = propType[propName])) propName = "ADBE Vector {0} - {1}".re(propType, x); 
+            }
+
+            propName = new RegExp(propName || "ADBE Vector .*", 'g');
+            var c = this.property("Contents"), i = 0, k = 0;
+            while(++ i<c.numProperties+1) if(propName.test(c.property(i).matchName)) k++;
+        
+            return k;
+        },
+
+        grabProps : function()
+        // layer.grabProps("Group", "Shape"); => Group 1, Rectangle 1
+        {
+
+            var _TYPES = 
+            [
+                "Group",
+                "Shape",
+                "Graphic",
+                "Filter"
+            ];
+        
+            var T = arguments.slice(), A= [], i=-1;
+            var C = this.content;
+
+            while(++i<C.numProperties+1){
+                prop = C.property(i);
+                mn = prop.matchName.split('-')[0].trim().split(" ").pop();
+                if(types.includes(mn)) allProps.push(prop);
+            }
+        
+            return A;
+        },
+
+        area : function(t){
+
+            c = this.containingComp;
+            t = t || c.time;
+        
+            sr = this.sourceRectAtTime(t, false); //sourceRect
+            sc = this.transform.scale.value; //scale
+            
+            return Math.mult.apply(null,
+                ([sr.width, sr.height] ^ (sc/100))
+            );
+        },
+
+        alpha : function(t){
+        
+            c = this.containingComp;
+            t = t || c.time;
+
+            var sr = this.sourceRectAtTime(t, false),
+            sc = this.transform.scale;
+
+            var wh = (sc/100) * ([sr.width, sr.height] /2)
+
+            p = [
+                c.toWorld(this, [sr.left, 0])[0], 
+                c.toWorld(this, [0, sr.top])[1]
+            ] + wh;
+            
+            this.addProp("Effects/Color Control:cc").property("Color").expression = (function()
+            {
+                thisLayer.sampleImage($p,$wh)
+            }).body({$p: p, $wh:wh});
+            
+            var rgba = this.getProp("Effects/cc").property("Color").value;
+            this.removeProp("Effects/cc");
+            return rgba[3];
+        },
+
+        areas : function(roundit){
+            
+            var areas     = [],
+                isEnabled = [],
+                contents  = this.property("Contents"),
+                numProps  = contents.numProperties,
+                i         = 0;
+        
+            while(++i < numProps+1)
+            {
+                isEnabled.push(contents.property(i).enabled);
+                contents.property(i).enabled = false;
+            }   
+            
+            i = 0;
+            while (++i< numProps+1)
+            {
+                if(i == 1)
+                {
+                    contents.property(i).enabled = true;
+                    currArea = this.area() * this.alpha();
+                    areas.push(roundit?Math.round(currArea):currArea);
+                    continue;
+                }
+
+                contents.property(i-1).enabled = false;
+                contents.property(i+0).enabled = true;
+
+                currArea = this.area() * this.alpha();
+                areas.push(roundit?Math.round(currArea): currArea);
+            }   i = 0;
+        
+            while(++i < numProps+1)
+            {
+                contents.property(i).enabled = isEnabled[i-1];
+            }
+        
+            return areas;
+        },
+
+        distances : function(origin)
+        {
+        
+            prop = function(c, n, pp)
+            {
+                return c.property(n)
+                        .property("Transform")
+                        .property(pp).value;
+            }
+            num = function(v){
+                return new Number(v);
+            }
+        
+            funcs = {
+        
+                topleft: function(pos){
+                    return Math.sqrt(
+                                    (num(pos[0] - src.left) ^ 2) 
+                                    + (num(pos[1] - src.top ) ^ 2)
+                            );
+                },
+                left: function(pos){
+                    return pos[0] - src.left;
+                },
+                right: function(pos){
+                    return wd - (pos[0] - src.left);
+                },
+                top: function(pos){
+                    return pos[1] - src.top;
+                },
+                bottom: function(pos){
+                    return ht - (pos[1] - src.top);
+                },
+                custom: function(pos){
+                    // TODO: Figure out the position coordinates of the
+                    // elements relative to the comp coordinate system.
+                    
+                    // Then simply subtract the distances to figure which
+                    // ones are closer than others.
+                }
+            }
+        
+            var positions  = [],
+                origin     = origin || "topleft";
+                contents   = this.property("Contents"),
+                numProps   = contents.numProperties;
+                
+            var src = this.sourceRectAtTime(this.containingComp.time, 0);
+                wd  = src.width;
+                ht  = src.height;
+        
+            for(i=0;++i<numProps+1;) positions.push(prop(contents,i, "Position"))
+        
+            return positions.map(funcs[origin]);    
+        }
+    })
--- src/AFFX/TextLayer.jsx
+++ src/AFFX/TextLayer.jsx
@@ -1,28 +1,28 @@
 
-TextLayer.prototype.xt({
+TextLayer
 
-    config: function(cfg)
-    {
-        var prop = textLayer.property("Source Text");
-        var doc = textProp.value;
-        doc.applyFill = cfg.applyFill;
-        doc.fontSize = cfg.fontSize;
-        doc.font = cfg.font;
-        doc.fillColor = cfg.fill;
-        prop.setValue(doc);
+    [PROTO]
+    ({
+        __name__: "SETTERS",
+        
+        config: function(cfg)
+        {
+            var prop = textLayer.property("Source Text");
+            var doc = textProp.value;
+            doc.applyFill = cfg.applyFill;
+            doc.fontSize = cfg.fontSize;
+            doc.font = cfg.font;
+            doc.fillColor = cfg.fill;
+            prop.setValue(doc);
 
-        return this;
-    },
+            return this;
+        },
+    )}
 
-    animator: function(name)
-    {
-        var am = this.Text.Animators.addProperty("ADBE Text Animator");
-        am.name = name || "animator"; 
-        return am;
-    },
-
-    fromJSONAndMarkersOf: function(layerName, jsonDataName, dataPointName)
-    {/**Expression to apply to a text layer source:
+    [PROTO]
+    ({
+        __name__: "EXPRESSIONS",
+        /**Expression to apply to a text layer source:
         * 
         * Example JSON:
         * 
@@ -44,24 +44,38 @@ TextLayer.prototype.xt({
         * (text = "move ball down")
         * <>   <>   |
         */
+        fromJSONAndMarkersOf: function(layerName, jsonDataName, dataPointName)
+        //@@requires ["AFFX.Expression"]
+        {
+            this.sourceText.expression = new Expression(function(){
 
-    this.sourceText.expression =  (function(){
+                var m = thisComp.layer($layerName).marker;
+                var t = time;
+                var i = m.nearestKey(time).index;
+            
+                if(m.nearestKey(t).time>t){ i--; } //if: |  <>
+                i || (i = 1);
+            
+                var obj = footage($footageName).sourceData;
 
-        var m = thisComp.layer($layerName).marker;
-        var t = time;
-        var i = m.nearestKey(time).index;
-    
-        if(m.nearestKey(t).time>t){ i--; } //if: |  <>
-        i || (i = 1);
-    
-        var obj = footage($footageName).sourceData;
+                obj[i][$dataPointName];
+                
+            },{
+                $layerName: layerName,
+                $footageName: jsonDataName,
+                $dataName : dataPointName
+            });
+        }
+    })
 
-        obj[i][$dataPointName];
+    [PROTO]
+    ({
+        __name__: "WRAPPERS",
         
-    }).body({
-        $layerName: layerName,
-        $footageName: jsonDataName,
-        $dataName : dataPointName
-    });
-    },
-})
+        animator: function(name)
+        {
+            var am = this.Text.Animators.addProperty("ADBE Text Animator");
+            am.name = name || "animator"; 
+            return am;
+        }
+    })

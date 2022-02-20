c70e0ccdda2dec6de4632866523bc9d959918f16
yahia boudah <yahiabouda@hotmail.com>
2022-02-20 20:24:04 +0100

    update HTTP

--- src/DATA/Folder.jsx
+++ src/DATA/Folder.jsx
@@ -1,71 +1,78 @@
-// [OPERATOR OVERLOADING]
-Folder.prototype.xt({
 
-    '/': function(op)
-    {
-        var pp = '{0}/{1}'.re(this.fsName, op);
-        return op.split('.').length? File(pp): Folder(pp);
-    }
-})
+Folder
 
-// [REMOVERS/ CLEANERS]
-Folder.prototype.xt({
-                    
-    clearFolder : function(extens)
-    {
-        var FD  = this,
-            ALL = is(extens, undefined)?1:0;
+    [PROTO]
+    ({
+        __name__: "OPOVR",
 
-        var FFS = FD.getFiles(), E, F;
-        for(x in FFS) if(x.in(FFS))
+        '/': function(op)
         {
-            F = FFS[x];
-            E = F.fsName.split('.');
-            E = E[E.length-1];
-
-            if(F.is(File) && (A || E == extens)) F.remvoe();
+            var pp = '{0}/{1}'.re(this.fsName, op);
+            return op.split('.').length? File(pp): Folder(pp);
         }
+    })
 
-        return FD;
-    }
-})
+    [PROTO]
+    ({
+        __name__: "CLEANERS",
 
-Folder.prototype.xt({
-
-    getF: function(what)
-    {
-        var cfg = 
+        clearFolder : function(extens)
         {
-            olders: Folder,
-            iles:   File
-        }
+            var FD  = this,
+                ALL = is(extens, undefined)?1:0;
 
-        var FD = this,
-            A  = [];
-
-        var FFS = FD.getFiles(), F;
-        for(x in FFS) if(x.in(FFS))
-        {
-            F = FFS[x];
-            
-            if(F.is(cfg[what])) A.push(F);
-        }
-
-        return A; 
-    },
-
-    getMostRecent: function()
-    {
-        var FD  = this,
-            FFS = FD.getFiles(), F, MR = -99999;
-        
-        for(x in FFS) if(x.in(FFS))
-        {
-            F = FFS[x];
-            if(F.modified > (FFS[x+1] || -9999))
+            var FFS = FD.getFiles(), E, F;
+            for(x in FFS) if(x.in(FFS))
             {
-                MR = F.modified;
+                F = FFS[x];
+                E = F.fsName.split('.');
+                E = E[E.length-1];
+
+                if(F.is(File) && (A || E == extens)) F.remvoe();
+            }
+
+            return FD;
+        }
+    })
+
+    [PROTO]
+    ({
+        __name__: "GETTERS",
+        
+        getF: function(what)
+        {
+            var cfg = 
+            {
+                olders: Folder,
+                iles:   File
+            }
+
+            var FD = this,
+                A  = [];
+
+            var FFS = FD.getFiles(), F;
+            for(x in FFS) if(x.in(FFS))
+            {
+                F = FFS[x];
+                
+                if(F.is(cfg[what])) A.push(F);
+            }
+
+            return A; 
+        },
+
+        getMostRecent: function()
+        {
+            var FD  = this,
+                FFS = FD.getFiles(), F, MR = -99999;
+            
+            for(x in FFS) if(x.in(FFS))
+            {
+                F = FFS[x];
+                if(F.modified > (FFS[x+1] || -9999))
+                {
+                    MR = F.modified;
+                }
             }
         }
-    }
-})
+    })

d75c8806c0db9a3b7d414ec77d23fb4ae1f2a000
yahia boudah <yahiabouda@hotmail.com>
2022-04-25 04:47:28 +0100

    funny huh

--- compiler/test.jsx
+++ compiler/test.jsx
@@ -2,4 +2,11 @@
 
 //@include "tokenizer.jsx"
 
+var x = -14;
 
+// the else is mapped to the closest possible
+// if statement
+
+if(x>0)
+    if(x>20) $.writeln("x greater than 20")
+else $.writeln("x is less than 20")
